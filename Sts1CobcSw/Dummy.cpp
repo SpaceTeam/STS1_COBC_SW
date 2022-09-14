d8c2cc19cc4beeda8abb6ba16c92728575762fee
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-09-14 19:17:51 +0200

    Rework project layout; part 1: source directory

---
+++ Sts1CobcSw/Dummy.cpp
@@ -0,0 +1,9 @@
+#include <Sts1CobcSw/Dummy.hpp>
+
+
+namespace sts1cobcsw
+{
+Dummy::Dummy() : name("Sts1CobcSw")
+{
+}
+}
---
+++ Sts1CobcSw/HelloDummy.cpp
@@ -0,0 +1,22 @@
+#include <Sts1CobcSw/Dummy.hpp>
+
+#include <rodos.h>
+
+
+namespace sts1cobcsw
+{
+class HelloDummy : public StaticThread<>
+{
+    void run() override
+    {
+        auto const dummy = Dummy();
+
+        TIME_LOOP(0, 500 * MILLISECONDS)
+        {
+            PRINTF("Hello %s!\n", dummy.name.data());
+        }
+    }
+};
+
+auto const helloDummy = HelloDummy();
+}
