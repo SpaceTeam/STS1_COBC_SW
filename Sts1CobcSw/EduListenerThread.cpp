27cf28a4502115b25f69bf18b13b168da21687aa
Jerome Hue <hue.jerome@yahoo.fr>
2022-09-18 22:04:01 +0200

    Add Utility.hpp from rodos_playground

--- Sts1CobcSw/EduListenerThread.cpp
+++ Sts1CobcSw/EduListenerThread.cpp
@@ -1,27 +1,61 @@
-#include <Sts1CobcSw/Dummy.hpp>
+#include <Sts1CobcSw/Utility.hpp>
 
 #include <Sts1CobcSw/Hal/Gpio.hpp>
 #include <Sts1CobcSw/Hal/IoNames.hpp>
 #include <Sts1CobcSw/Hal/PinNames.hpp>
+#include <type_safe/narrow_cast.hpp>
+#include <type_safe/types.hpp>
 
 #include <rodos.h>
 
 
 namespace sts1cobcsw
 {
-auto eduUpdateGpio = HAL_GPIO(hal::eduUpdatePin);
+namespace ts = type_safe;
+using ts::operator""_usize;
+
+// TODO : Remove this when hal is merged
+constexpr auto pb1 = RODOS::GPIO_017;
+constexpr auto eduUpdatePin = pb1;
+auto eduUpdateGpio = HAL_GPIO(eduUpdatePin);
 
 class EduListenerThread : public StaticThread<>
 {
     void init() override
     {
-        hal::InitPin(eduUpdateGpio, hal::PinType::input, hal::PinVal::one);
+        // TODO uncomment next line and remove the following when hal is merged
+        // hal::InitPin(eduUpdateGpio, hal::PinType::input, hal::PinVal::one);
+        eduUpdateGpio.init(/*isOutput=*/false, 1U, 0U);
     }
 
     void run() override
     {
-        if(true /*eduHasUpdate*/) {
-            // TODO Communicate with EDU
+        auto const eduHasUpdate = eduUpdateGpio.readPins() != 0;
+
+        if(eduHasUpdate)
+        {
+            // Communicate with EDU
+
+            // TODO this will be implemented by Daniel, will look something like
+            // auto status = periphery::edu::GetStatus();
+
+            // Possible outputs are : 
+            //      - No event
+            //      - Program Finished : Program-ID, Queue-ID and exit code are provided
+            //      - Results Ready : Program-ID, Queue-ID are provided
+            
+            auto programHasFinished = true;
+            auto event = true;
+            if(programHasFinished) {
+                // Update newest Status_and_History
+                // Resume EDU Queue thread
+            } else if(event) {
+
+                // What need get result file
+                // This is done with the get result command
+
+                // Interpret the result file and update the resulting SaHEntry from 2 or 3 to 4.
+            }
         }
 
         AT(NOW() + 1 * SECONDS);
