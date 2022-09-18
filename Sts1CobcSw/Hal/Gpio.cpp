26eaa720b36d6648e9b29731081c0a97613bac5f
danielschloms <daniel.schloms@gmail.com>
2022-09-18 17:55:03 +0200

    Apply formatting

--- Sts1CobcSw/Hal/Gpio.cpp
+++ Sts1CobcSw/Hal/Gpio.cpp
@@ -1,15 +1,19 @@
 #include <Sts1CobcSw/Hal/Gpio.hpp>
+
 #include <rodos.h>
 
-namespace sts1cobcsw::hal{
-    auto InitPin(RODOS::HAL_GPIO &pin, PinType pinType, PinVal initVal) -> int32_t{
-        bool isOutput = (pinType == PinType::output);
-        uint32_t initValUint = (initVal == PinVal::zero ? 0 : 1);
-        return pin.init(isOutput, 1, initValUint);
-    }
+namespace sts1cobcsw::hal
+{
+auto InitPin(RODOS::HAL_GPIO & pin, PinType pinType, PinVal initVal) -> int32_t
+{
+    bool isOutput = (pinType == PinType::output);
+    uint32_t initValUint = (initVal == PinVal::zero ? 0 : 1);
+    return pin.init(isOutput, 1, initValUint);
+}
 
-    void SetPin(RODOS::HAL_GPIO &pin, PinVal pinVal){
-        uint32_t pinValUint = (pinVal == PinVal::zero ? 0 : 1);
-        pin.setPins(pinValUint);
-    }
+void SetPin(RODOS::HAL_GPIO & pin, PinVal pinVal)
+{
+    uint32_t pinValUint = (pinVal == PinVal::zero ? 0 : 1);
+    pin.setPins(pinValUint);
+}
 }
--- Sts1CobcSw/Hal/Gpio.hpp
+++ Sts1CobcSw/Hal/Gpio.hpp
@@ -1,43 +1,48 @@
 #pragma once
 
 #include <Sts1CobcSw/Hal/PinNames.hpp>
-#include <cstdint>
+
 #include <rodos.h>
+
+#include <cstdint>
 
 namespace sts1cobcsw::hal
 {
-    /**
-     * @brief The pin types of a GPIO pin
-     */
-    enum class PinType{
-        input,
-        output
-    };
+/**
+ * @brief The pin types of a GPIO pin
+ */
+enum class PinType
+{
+    input,
+    output
+};
 
-    /**
-     * @brief Possible values of a single GPIO pin
-     */
-    enum class PinVal{
-        zero,
-        one
-    };
+/**
+ * @brief Possible values of a single GPIO pin
+ */
+enum class PinVal
+{
+    zero,
+    one
+};
 
-    /**
-     * @brief A wrapper for the init function of a GPIO pin. Only allows for single pins to be initialized
-     * 
-     * @param pin The pin to be initialized
-     * @param pinType The pin type (input/output)
-     * @param initVal The initial value for output pins (zero/one)
-     * 
-     * @returns Returns 0 on success, -1 on failure
-     */
-    auto InitPin(RODOS::HAL_GPIO &pin, PinType pinType, PinVal initVal) -> int32_t;
+/**
+ * @brief A wrapper for the init function of a GPIO pin. Only allows for single pins to be
+ * initialized
+ *
+ * @param pin The pin to be initialized
+ * @param pinType The pin type (input/output)
+ * @param initVal The initial value for output pins (zero/one)
+ *
+ * @returns Returns 0 on success, -1 on failure
+ */
+auto InitPin(RODOS::HAL_GPIO & pin, PinType pinType, PinVal initVal) -> int32_t;
 
-    /**
-     * @brief A wrapper for the setPins function of a GPIO pin.
-     * 
-     * @param pin The pin to be set
-     * @param pinVal The value to be set (zero/one)
-     */
-    void SetPin(RODOS::HAL_GPIO &pin, PinVal pinVal);
+/**
+ * @brief A wrapper for the setPins function of a GPIO pin.
+ *
+ * @param pin The pin to be set
+ * @param pinVal The value to be set (zero/one)
+ */
+void SetPin(RODOS::HAL_GPIO & pin, PinVal pinVal);
 }
