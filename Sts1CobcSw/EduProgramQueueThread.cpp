a73f82a06e58b34bc3ae223784be2cbf0288dbf9
Jerome Hue <hue.jerome@yahoo.fr>
2022-10-01 13:17:01 +0200

    Add status and history ring buffer

--- Sts1CobcSw/EduProgramQueueThread.cpp
+++ Sts1CobcSw/EduProgramQueueThread.cpp
@@ -1,6 +1,9 @@
 #include <Sts1CobcSw/EduProgramQueueThread.hpp>
 
 #include <Sts1CobcSw/Periphery/Edu.hpp>
+#include <Sts1CobcSw/Periphery/Enums.hpp>
+
+#include <ringbuffer.h>
 
 #include <rodos_no_using_namespace.h>
 
@@ -10,16 +13,20 @@
 
 namespace sts1cobcsw
 {
+using RODOS::SECONDS;
+// Define our Edu Program Queue
 etl::vector<QueueEntry, eduProgramQueueSize> eduQueue;
 
-auto AddQueueEntry(const std::tuple<int16_t, int16_t, int64_t, int64_t> & eduEntry)
+RODOS::RingBuffer<StatusHistoryEntry, statusHistorySize> statusHistory;
+
+auto AddQueueEntry(const QueueEntry & eduEntry)
 {
     eduQueue.push_back(eduEntry);
 }
 
 auto eduUartInterface = periphery::EduUartInterface();
 
-auto currentQueueId = 0;
+auto currentQueueId = 0U;
 
 class EduQueueThread : public RODOS::StaticThread<>
 {
@@ -43,22 +50,28 @@ class EduQueueThread : public RODOS::StaticThread<>
         RODOS::AT(RODOS::NOW() + delayTime - 2 * RODOS::SECONDS);
 
         // TODO : Send UTC estimation date to edu
+        // eduUartInterface.updateTime();
 
         RODOS::AT(RODOS::NOW() + delayTime);
 
         // Start Process
-        auto const programID = std::get<0>(eduQueue.at(0));
-        auto const queueID = std::get<1>(eduQueue.at(0));
-        auto const timeout = std::get<3>(eduQueue.at(0));
+        auto const programId = std::get<0>(eduQueue.at(currentQueueId));
+        auto const queueId = std::get<1>(eduQueue.at(currentQueueId));
+        auto const timeout = std::get<3>(eduQueue.at(currentQueueId));
 
         // Execute Program
-        eduUartInterface.ExecuteProgram(programID, queueID, timeout);
+        auto eduAnswer = eduUartInterface.ExecuteProgram(programId, queueId, timeout);
+
+        etl::string<statusMaxSize> status = "1x";
+        if(eduAnswer == periphery::EduErrorCode::success)
+        {
+            status = "1";
+        }
         // TODO Create a status and history entry if not already existing
-        // TODO Set status =1 if RasPi answers with success else 1x
+        statusHistory.put(std::make_tuple(programId, queueId, status));
 
-        // Suspend Self until exetime
-        RODOS::AT(RODOS::NOW() + timeout * RODOS::SECONDS + 2 * RODOS::SECONDS);
-
+        // Suspend Self until exetime = now() + timeout + 2 sec
+        RODOS::AT(RODOS::NOW() + (timeout + 2) * SECONDS);
 
         // Set current Queue ID to next
         currentQueueId++;
--- Sts1CobcSw/EduProgramQueueThread.hpp
+++ Sts1CobcSw/EduProgramQueueThread.hpp
@@ -2,18 +2,28 @@
 
 #include <rodos_no_using_namespace.h>
 
+#include <etl/string.h>
+
 #include <tuple>
 
 
 namespace sts1cobcsw
 {
+constexpr auto eduProgramQueueSize = 20;
+constexpr auto statusHistorySize = 20;
+
+
 // A queue consists of :
 // Program ID 	: 2 bytes, according to EDU PDD 6.1.1
 // Queue ID 	: 2 bytes, according to EDU PDD 6.1.2
 // Start Time 	: 4 bytes, EPOCH time
 // Timeout 		: 2 bytes, according to EDU PDD 6.1.2
-constexpr auto eduProgramQueueSize = 20;
-
-
 using QueueEntry = std::tuple<uint16_t, uint16_t, uint32_t, uint16_t>;
+
+// A stus and history entry consists of :
+// Progam ID 	: 2 bytes
+// Queue ID		: 2 bytes
+// Status		: A string ? with max 2 characters
+constexpr auto statusMaxSize = 2;
+using StatusHistoryEntry  = std::tuple<uint16_t, uint16_t, etl::string<statusMaxSize>>;
 }
