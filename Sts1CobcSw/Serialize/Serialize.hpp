9349461baf8b59c8d587694ae22b1284b6336edb
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-10-18 10:46:02 +0200

    Serialize TriviallySerializable types

--- Sts1CobcSw/Serialize/Serialize.hpp
+++ Sts1CobcSw/Serialize/Serialize.hpp
@@ -1,4 +1,6 @@
+#include <array>
 #include <concepts>
+#include <cstring>
 #include <type_traits>
 
 
@@ -6,4 +8,34 @@ namespace sts1cobcsw::serialize
 {
 template<typename T>
 concept TriviallySerializable = std::is_arithmetic<T>::value or std::is_enum<T>::value;
+
+
+constexpr auto SerialSize(TriviallySerializable auto data)
+{
+    return sizeof(data);
+}
+
+
+template<std::size_t size>
+using ByteArray = std::array<std::byte, size>;
+
+template<typename T>
+using SerialBuffer = ByteArray<SerialSize(T())>;
+
+
+template<TriviallySerializable T>
+constexpr auto SerializeTo(std::byte * destination, T t)
+{
+    std::memcpy(destination, &t, SerialSize(t));
+    return destination + SerialSize(t);  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+}
+
+
+template<TriviallySerializable T>
+constexpr auto Serialize(T t)
+{
+    auto buffer = SerialBuffer<T>{};
+    SerializeTo(buffer.data(), t);
+    return buffer;
+}
 }
--- Tests/UnitTests/Serialize.test.cpp
+++ Tests/UnitTests/Serialize.test.cpp
@@ -7,6 +7,9 @@
 #include <cstdint>
 
 
+using sts1cobcsw::serialize::Serialize;
+
+
 TEST_CASE("TriviallySerializable")
 {
     using sts1cobcsw::serialize::TriviallySerializable;
@@ -38,4 +41,31 @@ TEST_CASE("TriviallySerializable")
     // User-defined types aren't either
     REQUIRE(not TriviallySerializable<EmptyStruct>);
     REQUIRE(not TriviallySerializable<SingleInt32>);
+    // TODO: Check type_safe::uint16_t et al.
+}
+
+
+TEST_CASE("Serialize TriviallySerializable types")
+{
+    auto b = std::byte{0xAA};
+    uint16_t u16 = 3;
+    int32_t i32 = -2;
+
+    auto bBuffer = Serialize(b);
+    auto u16Buffer = Serialize(u16);
+    auto i32Buffer = Serialize(i32);
+
+    REQUIRE(std::is_same<decltype(bBuffer), std::array<std::byte, sizeof(std::byte)>>::value);
+    REQUIRE(std::is_same<decltype(u16Buffer), std::array<std::byte, sizeof(uint16_t)>>::value);
+    REQUIRE(std::is_same<decltype(i32Buffer), std::array<std::byte, sizeof(int32_t)>>::value);
+
+    auto bIsCorrectlySerialized = bBuffer[0] == std::byte{0xAA};
+    auto u16IsCorrectlySerialized =
+        (u16Buffer[0] == std::byte{0x03} and u16Buffer[1] == std::byte{0x00});
+    auto i32IsCorrectlySerialized =
+        (i32Buffer[0] == std::byte{0xFE} and i32Buffer[1] == std::byte{0xFF}
+         and i32Buffer[2] == std::byte{0xFF} and i32Buffer[3] == std::byte{0xFF});
+    REQUIRE(bIsCorrectlySerialized);
+    REQUIRE(u16IsCorrectlySerialized);
+    REQUIRE(i32IsCorrectlySerialized);
 }
