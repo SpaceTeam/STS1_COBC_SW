5e03a957c11de3f8b03d3569252d29cf08230a5f
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-10-24 17:41:35 +0200

    Rework Uart.test.cpp to only test UCI UART
    The old test was not usable without a working EDU so I changed it to
    just test reading from and writing to the UCI UART.

--- Tests/HardwareTests/Uart.test.cpp
+++ Tests/HardwareTests/Uart.test.cpp
@@ -1,13 +1,10 @@
 //! @file
-//! @brief A program to test reading from the UCI/EDU UART
+//! @brief A program to test writing to and reading from the UCI UART
 //!
 //! Preparation:
-//!     - Prepare any program that prints/reads to/from the EDU UART on the EDU
 //!     - Connect the UCI UART to a computer to use with HTERM, Putty, etc.
 //!
-//! After flashing the COBC, the program will
-//! 1) Reflect messages from the EDU and print them to the UCI UART
-//! 2) Reflect messages from the UCI
+//! After flashing the COBC just follow the instructions on the screen.
 
 #include <Sts1CobcSw/Hal/Communication.hpp>
 #include <Sts1CobcSw/Hal/GpioPin.hpp>
@@ -17,7 +14,9 @@
 #include <rodos_no_using_namespace.h>
 
 #include <array>
+#include <charconv>
 #include <cstddef>
+#include <cstdint>
 #include <span>
 
 
@@ -27,6 +26,17 @@ auto eduUart = RODOS::HAL_UART(hal::eduUartIndex, hal::eduUartTxPin, hal::eduUar
 auto uciUart = RODOS::HAL_UART(hal::uciUartIndex, hal::uciUartTxPin, hal::uciUartRxPin);
 
 
+template<std::size_t nDigits = 1>
+auto ToChars(int i)
+{
+    auto string = std::array<char, nDigits>{};
+    std::to_chars(data(string), data(string) + size(string), i);
+    return string;
+}
+
+
+// TODO: Make this test usable even if there is no EDU connected/working. This means two threads,
+// one for EDU one for UCI and it should use suspendUntilDataReady() and maybe a timeout, etc.
 class UartTest : public RODOS::StaticThread<>
 {
     void init() override
@@ -39,20 +49,21 @@ class UartTest : public RODOS::StaticThread<>
 
     void run() override
     {
+        hal::WriteTo(&uciUart, "Hello UCI UART!\n");
+
         while(true)
         {
-            constexpr auto bufferSize = 64;
+            constexpr auto bufferSize = 5;
+            auto buffer = std::array<std::byte, bufferSize>{};
 
-            // Check EDU UART
-            auto eduBuffer = std::array<std::byte, bufferSize>{};
-            hal::ReadFrom(&eduUart, std::span(eduBuffer));
-            hal::WriteTo(&eduUart, std::span(eduBuffer));
-            hal::WriteTo(&uciUart, std::span(eduBuffer));
-
-            // Check UCI UART
-            auto uciBuffer = std::array<std::byte, bufferSize>{};
-            hal::ReadFrom(&uciUart, std::span(uciBuffer));
-            hal::WriteTo(&uciUart, std::span(uciBuffer));
+            hal::WriteTo(&uciUart, "\nPlease send ");
+            auto bufferSizeString = ToChars(bufferSize);
+            hal::WriteTo(&uciUart, std::span(bufferSizeString));
+            hal::WriteTo(&uciUart, " characters\n");
+            hal::ReadFrom(&uciUart, std::span(buffer));
+            hal::WriteTo(&uciUart, "You sent: ");
+            hal::WriteTo(&uciUart, std::span(buffer));
+            hal::WriteTo(&uciUart, "\n");
         }
     }
 };
