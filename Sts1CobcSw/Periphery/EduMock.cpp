497bc2d54be8799359aade80ada99b1a116a4ee8
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-10-27 11:44:10 +0200

    Update EduMock.cpp to new Edu API

--- Sts1CobcSw/Periphery/EduMock.cpp
+++ Sts1CobcSw/Periphery/EduMock.cpp
@@ -4,51 +4,46 @@
 #include <Sts1CobcSw/Utility/Crc32.hpp>
 #include <Sts1CobcSw/Utility/UtilityNames.hpp>
 
-#include <array>
-#include <iostream>
-#include <vector>
 
 namespace sts1cobcsw::periphery
 {
-EduUartInterface::EduUartInterface() = default;
-
-// NOLINTNEXTLINE
-void EduUartInterface::FlushUartBuffer()
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 {
     RODOS::PRINTF("Call to FlushUartBuffer()\n");
 }
 
-// NOLINTNEXTLINE
-auto EduUartInterface::UartReceive(std::vector<uint8_t> & recvVec, size_t nBytes) -> EduErrorCode
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+[[nodiscard]] auto Edu::UartReceive([[maybe_unused]] std::span<Byte> destination) -> EduErrorCode
 {
     RODOS::PRINTF("Call to UartReceive()\n");
     return EduErrorCode::success;
 }
 
-// NOLINTNEXTLINE
-void EduUartInterface::SendCommand(uint8_t cmd)
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+auto Edu::SendCommand([[maybe_unused]] Byte commandId) -> void
 {
     RODOS::PRINTF("Call to SendCommand()\n");
 }
 
-// NOLINTNEXTLINE
-auto EduUartInterface::SendData(std::span<uint8_t> data) -> EduErrorCode
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+[[nodiscard]] auto Edu::SendData([[maybe_unused]] std::span<Byte> data) -> EduErrorCode
 {
     RODOS::PRINTF("Call to SendData()\n");
     return EduErrorCode::success;
 }
 
-// NOLINTNEXTLINE
-auto EduUartInterface::ExecuteProgram(uint16_t programId, uint16_t queueId, uint16_t timeout)
-    -> EduErrorCode
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+[[nodiscard]] auto Edu::ExecuteProgram([[maybe_unused]] ExecuteProgramData const & data) -> EduErrorCode
 {
     RODOS::PRINTF("Call to ExecuteProgram()\n");
     return EduErrorCode::success;
 }
 
-// NOLINTNEXTLINE
-auto EduUartInterface::GetStatus()
-    -> std::tuple<EduStatusType, uint16_t, uint16_t, uint8_t, EduErrorCode>
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 {
     RODOS::PRINTF("Call to GetStatus()\n");
 
