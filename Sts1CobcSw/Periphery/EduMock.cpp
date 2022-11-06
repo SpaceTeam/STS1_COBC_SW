3b89af4da8008f93f90790c5d1b14df531b901ad
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-11-06 01:16:37 +0100

    Add PrintTime() to every function call in EduMock.cpp

--- Sts1CobcSw/Periphery/EduMock.cpp
+++ Sts1CobcSw/Periphery/EduMock.cpp
@@ -2,17 +2,20 @@
 #include <Sts1CobcSw/Periphery/Edu.hpp>
 #include <Sts1CobcSw/Periphery/EduNames.hpp>
 #include <Sts1CobcSw/Utility/Crc32.hpp>
+#include <Sts1CobcSw/Utility/Time.hpp>
 #include <Sts1CobcSw/Utility/UtilityNames.hpp>
 
 
 namespace sts1cobcsw
 {
 using RODOS::PRINTF;
+using utility::PrintTime;
 
 
 // TODO: Move this in the proper file
 auto ResumeEduErrorCommunicationThread() -> void
 {
+    PrintTime();
     PRINTF("Call to ResumeEduErrorCommunicationThread()\n");
 }
 
@@ -23,6 +26,7 @@ namespace periphery
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 auto Edu::Initialize() -> void
 {
+    PrintTime();
     PRINTF("Call to Initialize()\n");
 }
 
@@ -30,6 +34,7 @@ auto Edu::Initialize() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 auto Edu::TurnOn() -> void
 {
+    PrintTime();
     PRINTF("Call to TurnOn()\n");
 }
 
@@ -37,6 +42,7 @@ auto Edu::TurnOn() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 auto Edu::TurnOff() -> void
 {
+    PrintTime();
     PRINTF("Call to TurnOff()\n");
 }
 
@@ -44,6 +50,7 @@ auto Edu::TurnOff() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::StoreArchive(StoreArchiveData const & data) -> std::int32_t
 {
+    PrintTime();
     PRINTF("Call to StoreArchive(programId = %d)\n", data.programId.get());
     return 0;
 }
@@ -52,6 +59,7 @@ auto Edu::TurnOff() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::ExecuteProgram(ExecuteProgramData const & data) -> EduErrorCode
 {
+    PrintTime();
     PRINTF("Call to ExecuteProgram(programId = %d, queueId = %d, timeout = %d)\n",
            data.programId.get(),
            data.queueId.get(),
@@ -63,6 +71,7 @@ auto Edu::TurnOff() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::StopProgram() -> EduErrorCode
 {
+    PrintTime();
     PRINTF("Call to StopProgram()\n");
     return EduErrorCode::success;
 }
@@ -71,8 +80,8 @@ auto Edu::TurnOff() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::GetStatus() -> EduStatus
 {
+    PrintTime();
     PRINTF("Call to GetStatus()\n");
-
     return {.statusType = EduStatusType::invalid,
             .programId = 0,
             .queueId = 0,
@@ -84,6 +93,7 @@ auto Edu::TurnOff() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::UpdateTime(UpdateTimeData const & data) -> EduErrorCode
 {
+    PrintTime();
     PRINTF("Call to UpdateTime(timestamp = %d)\n", data.timestamp.get());
     return EduErrorCode::success;
 }
@@ -92,6 +102,7 @@ auto Edu::TurnOff() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 auto Edu::SendCommand(Byte commandId) -> void
 {
+    PrintTime();
     PRINTF("Call to SendCommand(commandId = 0x%02x)\n", static_cast<unsigned int>(commandId));
 }
 
@@ -99,6 +110,7 @@ auto Edu::SendCommand(Byte commandId) -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::SendData(std::span<Byte> data) -> EduErrorCode
 {
+    PrintTime();
     PRINTF("Call to SendData(size(data) = %d)\n", size(data));
     return EduErrorCode::success;
 }
@@ -107,6 +119,7 @@ auto Edu::SendCommand(Byte commandId) -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::UartReceive([[maybe_unused]] std::span<Byte> destination) -> EduErrorCode
 {
+    PrintTime();
     PRINTF("Call to UartReceive(size(destination) = %d)\n", size(destination));
     return EduErrorCode::success;
 }
@@ -115,6 +128,7 @@ auto Edu::SendCommand(Byte commandId) -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 void Edu::FlushUartBuffer()
 {
+    PrintTime();
     PRINTF("Call to FlushUartBuffer()\n");
 }
 }
