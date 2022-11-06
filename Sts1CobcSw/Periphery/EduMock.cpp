982fa893999197b588fed3e383f2946ce562bdd9
Patrick Kappl <patrick.kappl@spaceteam.at>
2022-11-06 01:02:25 +0100

    Add missing EduMock functions and print arguments

--- Sts1CobcSw/Periphery/EduMock.cpp
+++ Sts1CobcSw/Periphery/EduMock.cpp
@@ -7,28 +7,63 @@
 
 namespace sts1cobcsw
 {
+using RODOS::PRINTF;
+
+
 // TODO: Move this in the proper file
 auto ResumeEduErrorCommunicationThread() -> void
 {
-    RODOS::PRINTF("Call to EduErrorCommunicationTHread");
+    PRINTF("Call to ResumeEduErrorCommunicationThread()\n");
 }
 
-}
 
 // TODO: This file is not used at all right now. Think about the mocking later.
-namespace sts1cobcsw::periphery
+namespace periphery
 {
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 auto Edu::Initialize() -> void
 {
-    RODOS::PRINTF("Call to Initialize()\n");
+    PRINTF("Call to Initialize()\n");
 }
 
 
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-[[nodiscard]] auto Edu::ExecuteProgram([[maybe_unused]] ExecuteProgramData const & data)
-    -> EduErrorCode
+auto Edu::TurnOn() -> void
 {
-    RODOS::PRINTF("Call to ExecuteProgram()\n");
+    PRINTF("Call to TurnOn()\n");
+}
+
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+auto Edu::TurnOff() -> void
+{
+    PRINTF("Call to TurnOff()\n");
+}
+
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+[[nodiscard]] auto Edu::StoreArchive(StoreArchiveData const & data) -> std::int32_t
+{
+    PRINTF("Call to StoreArchive(programId = %d)\n", data.programId.get());
+    return 0;
+}
+
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+[[nodiscard]] auto Edu::ExecuteProgram(ExecuteProgramData const & data) -> EduErrorCode
+{
+    PRINTF("Call to ExecuteProgram(programId = %d, queueId = %d, timeout = %d)\n",
+           data.programId.get(),
+           data.queueId.get(),
+           data.timeout.get());
+    return EduErrorCode::success;
+}
+
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+[[nodiscard]] auto Edu::StopProgram() -> EduErrorCode
+{
+    PRINTF("Call to StopProgram()\n");
     return EduErrorCode::success;
 }
 
@@ -36,7 +71,7 @@ auto Edu::Initialize() -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::GetStatus() -> EduStatus
 {
-    RODOS::PRINTF("Call to GetStatus()\n");
+    PRINTF("Call to GetStatus()\n");
 
     return {.statusType = EduStatusType::invalid,
             .programId = 0,
@@ -47,24 +82,24 @@ auto Edu::Initialize() -> void
 
 
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-[[nodiscard]] auto Edu::UpdateTime([[maybe_unused]] UpdateTimeData const & data) -> EduErrorCode
+[[nodiscard]] auto Edu::UpdateTime(UpdateTimeData const & data) -> EduErrorCode
 {
-    RODOS::PRINTF("Call to UpdateTime()\n");
+    PRINTF("Call to UpdateTime(timestamp = %d)\n", data.timestamp.get());
     return EduErrorCode::success;
 }
 
 
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-auto Edu::SendCommand([[maybe_unused]] Byte commandId) -> void
+auto Edu::SendCommand(Byte commandId) -> void
 {
-    RODOS::PRINTF("Call to SendCommand()\n");
+    PRINTF("Call to SendCommand(commandId = 0x%02x)\n", static_cast<unsigned int>(commandId));
 }
 
 
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-[[nodiscard]] auto Edu::SendData([[maybe_unused]] std::span<Byte> data) -> EduErrorCode
+[[nodiscard]] auto Edu::SendData(std::span<Byte> data) -> EduErrorCode
 {
-    RODOS::PRINTF("Call to SendData()\n");
+    PRINTF("Call to SendData(size(data) = %d)\n", size(data));
     return EduErrorCode::success;
 }
 
@@ -72,13 +107,15 @@ auto Edu::SendCommand([[maybe_unused]] Byte commandId) -> void
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 [[nodiscard]] auto Edu::UartReceive([[maybe_unused]] std::span<Byte> destination) -> EduErrorCode
 {
-    RODOS::PRINTF("Call to UartReceive()\n");
+    PRINTF("Call to UartReceive(size(destination) = %d)\n", size(destination));
     return EduErrorCode::success;
 }
+
 
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 void Edu::FlushUartBuffer()
 {
-    RODOS::PRINTF("Call to FlushUartBuffer()\n");
+    PRINTF("Call to FlushUartBuffer()\n");
+}
 }
 }
