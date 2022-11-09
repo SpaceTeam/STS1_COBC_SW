cd32eb4e476e8340d0de68ccb153cb012625393c
Jerome Hue <hue.jerome@yahoo.fr>
2022-11-09 14:58:15 +0100

    Refactor thread testing
    Add a new script to run a thread and trim the rodos header.
    Add a new target for thread tests, called AllGoldenThreadTests.

--- cmake/golden-tests.cmake
+++ cmake/golden-tests.cmake
@@ -35,16 +35,10 @@ macro(add_thread_golden_test)
     set(multi_value_args FILES LIB)
     cmake_parse_arguments(GT "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-    message("Files : ${GT_FILES}")
-    message("Libs : ${GT_LIB}")
-
     list(GET GT_FILES 0 test_filename)
     get_filename_component(test_filename ${test_filename} NAME_WE)
 
     set(target_name ${PROJECT_NAME}_${test_filename})
-
-    message("Target name : ${PROJECT_NAME}_${test_filename}")
-    message("test_filename : ${test_filename}")
 
     add_executable(${target_name} EXCLUDE_FROM_ALL ${GT_FILES})
     target_include_directories(${target_name} ${warning_guard} PUBLIC "${CMAKE_SOURCE_DIR}")
@@ -53,21 +47,21 @@ macro(add_thread_golden_test)
 
     add_custom_command(
         OUTPUT "${test_filename}.output"
-        COMMAND bash "${CMAKE_CURRENT_SOURCE_DIR}/Scripts/TestRunner.sh"
+        COMMAND bash "${CMAKE_CURRENT_SOURCE_DIR}/Scripts/ThreadTestRunner.sh"
                 $<TARGET_FILE:${target_name}>
-        DEPENDS ${target_name} Scripts/TestRunner.sh
+        DEPENDS ${target_name} Scripts/ThreadTestRunner.sh
     )
+
+    list(APPEND thread_output_files "${test_filename}.output")
 
     add_custom_target(
         ${target_name}_Clean COMMAND ${CMAKE_COMMAND} -E remove -f "${test_filename}.output"
     )
 
-    if(FALSE) 
     add_test(NAME ${test_filename}_Test
-             COMMAND diff "${test_filename}.output"
+             COMMAND diff "${test_filename}.output.trimmed"
                      "${CMAKE_CURRENT_SOURCE_DIR}/ExpectedOutputs/${test_filename}.txt"
     )
-    endif()
 
 
 endmacro()
