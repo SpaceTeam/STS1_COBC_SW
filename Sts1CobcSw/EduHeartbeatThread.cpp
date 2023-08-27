ed660825adb94b1c03c24ac9e1a97eb0ed7b8d85
Patrick Kappl <kappl.pa@gmail.com>
2023-08-27 19:46:09 +0000

    Revert "Hack EduHeartbeatThread for HAF"
    This reverts commit 71fc2eeeb4723c49000615a1361e0c7c927b0b36.

--- Sts1CobcSw/EduHeartbeatThread.cpp
+++ Sts1CobcSw/EduHeartbeatThread.cpp
@@ -39,54 +39,49 @@ private:
     // TODO: It must be possible to simplify the logic for heartbeat detection
     void run() override
     {
-        // Hack for HAF
-        eduIsAliveTopic.publish(false);
-        RODOS::AT(RODOS::NOW() + 20 * RODOS::SECONDS);
-        eduIsAliveTopic.publish(true);
-        
-        // namespace ts = type_safe;
-        // using ts::operator""_i;
-        // using ts::operator""_isize;
+        namespace ts = type_safe;
+        using ts::operator""_i;
+        using ts::operator""_isize;
 
-        // constexpr auto heartbeatFrequency = 10_isize;                     // Hz
-        // constexpr auto samplingFrequency = 5_isize * heartbeatFrequency;  // Hz
-        // constexpr auto samplingPeriod = 1000_isize * MILLISECONDS / samplingFrequency;
-        // constexpr auto edgeCounterThreshold = 4;
+        constexpr auto heartbeatFrequency = 10_isize;                     // Hz
+        constexpr auto samplingFrequency = 5_isize * heartbeatFrequency;  // Hz
+        constexpr auto samplingPeriod = 1000_isize * MILLISECONDS / samplingFrequency;
+        constexpr auto edgeCounterThreshold = 4;
 
-        // auto edgeCounter = 0_i;
-        // auto samplingCount = 0_i;
-        // ts::bool_t heartbeatIsConstant = true;
-        // auto oldHeartbeat = eduHeartbeatGpioPin.Read();
+        auto edgeCounter = 0_i;
+        auto samplingCount = 0_i;
+        ts::bool_t heartbeatIsConstant = true;
+        auto oldHeartbeat = eduHeartbeatGpioPin.Read();
 
-        // TIME_LOOP(0, samplingPeriod.get())
-        // {
-        //     auto heartbeat = eduHeartbeatGpioPin.Read();
-        //     ++samplingCount;
+        TIME_LOOP(0, samplingPeriod.get())
+        {
+            auto heartbeat = eduHeartbeatGpioPin.Read();
+            ++samplingCount;
 
-        //     if(heartbeatIsConstant and (heartbeat != oldHeartbeat))
-        //     {
-        //         heartbeatIsConstant = false;
-        //         edgeCounter++;
-        //     }
-        //     if(edgeCounter == edgeCounterThreshold)
-        //     {
-        //         eduIsAliveTopic.publish(true);
-        //         edgeCounter = 0;
-        //     }
+            if(heartbeatIsConstant and (heartbeat != oldHeartbeat))
+            {
+                heartbeatIsConstant = false;
+                edgeCounter++;
+            }
+            if(edgeCounter == edgeCounterThreshold)
+            {
+                eduIsAliveTopic.publish(true);
+                edgeCounter = 0;
+            }
 
-        //     // Check if heartbeat is constant over a whole heartbeat period
-        //     if(samplingCount == samplingFrequency / heartbeatFrequency)
-        //     {
-        //         if(heartbeatIsConstant)
-        //         {
-        //             edgeCounter = 0;
-        //             eduIsAliveTopic.publish(false);
-        //         }
-        //         heartbeatIsConstant = true;
-        //         samplingCount = 0_i;
-        //     }
-        //     oldHeartbeat = heartbeat;
-        // }
+            // Check if heartbeat is constant over a whole heartbeat period
+            if(samplingCount == samplingFrequency / heartbeatFrequency)
+            {
+                if(heartbeatIsConstant)
+                {
+                    edgeCounter = 0;
+                    eduIsAliveTopic.publish(false);
+                }
+                heartbeatIsConstant = true;
+                samplingCount = 0_i;
+            }
+            oldHeartbeat = heartbeat;
+        }
     }
 } eduHeartbeatThread;
 }
