85dbd1e6afe2db7061eb9f72a840d522da4d4d00
Patrick Kappl <kappl.pa@gmail.com>
2023-09-10 16:12:14 +0000

    Signal matching device IDs separately in DeviceIds.test.cpp
    Previously the LED was only on if all IDs matched but it is of course
    more useful to know this information separately for all 3 peripheral
    devices (flash, FRAM, RF).

--- Tests/HardwareTests/DeviceIds.test.cpp
+++ Tests/HardwareTests/DeviceIds.test.cpp
@@ -14,7 +14,10 @@
 
 namespace sts1cobcsw
 {
+using RODOS::AT;
+using RODOS::NOW;
 using RODOS::PRINTF;
+using RODOS::SECONDS;
 using serial::operator""_b;
 
 
@@ -22,12 +25,12 @@ constexpr std::size_t stackSize = 5'000;
 
 std::int32_t flashErrorCode = 0;
 std::int32_t framErrorCode = 0;
-std::int32_t rfErrorCode = 0;
 
 auto ledGpioPin = hal::GpioPin(hal::led1Pin);
 
 
 auto PrintFramDeviceId(periphery::fram::DeviceId const & deviceId) -> void;
+auto BlinkLed(int frequency, int duration) -> void;
 
 
 class DeviceIdsTest : public RODOS::StaticThread<stackSize>
@@ -59,47 +62,55 @@ private:
         periphery::rf::Initialize(periphery::rf::TxType::morse);
         PRINTF("RF module initialized\n");
 
-        auto allIdsMatch = true;
-
         PRINTF("\nFlash:\n");
         auto correctManufacturerId = 0xEF;
         auto jedecId = periphery::flash::ReadJedecId();
         PRINTF("Manufacturer ID: 0x%02x == 0x%02x\n",
                static_cast<unsigned int>(jedecId.manufacturerId),
                static_cast<unsigned int>(correctManufacturerId));
-        allIdsMatch &= jedecId.manufacturerId == correctManufacturerId;
+        auto flashIdsMatch = jedecId.manufacturerId == correctManufacturerId;
 
         auto correctFlashDeviceId = 0x4021;
         PRINTF("Device ID: 0x%04x == 0x%04x\n",
                static_cast<unsigned int>(jedecId.deviceId),
                static_cast<unsigned int>(correctFlashDeviceId));
-        allIdsMatch &= jedecId.deviceId == correctFlashDeviceId;
+        flashIdsMatch &= jedecId.deviceId == correctFlashDeviceId;
 
         PRINTF("\nFRAM:\n");
         auto correctFramDeviceId = periphery::fram::DeviceId{
             0x03_b, 0x2E_b, 0xC2_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b};
-            // 0x00_b, 0x2C_b, 0xC2_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b};
+        // 0x00_b, 0x2C_b, 0xC2_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b, 0x7F_b};
         auto deviceId = periphery::fram::ReadDeviceId();
         PRINTF("Device ID: ");
         PrintFramDeviceId(deviceId);
         PRINTF(" ==\n           ");
         PrintFramDeviceId(correctFramDeviceId);
-        allIdsMatch &= deviceId == correctFramDeviceId;
+        auto framIdsMatch = deviceId == correctFramDeviceId;
 
         PRINTF("\nRF:\n");
         auto correctPartInfo = 0x4463;
         auto partInfo = periphery::rf::GetPartInfo();
         PRINTF("Part info: 0x%4x == 0x%4x\n", partInfo, correctPartInfo);
-        allIdsMatch &= partInfo == correctPartInfo;
+        auto rfIdsMatch = partInfo == correctPartInfo;
 
-        if(allIdsMatch)
+        auto blinkDuration = 3;            // s
+        auto flashBlinkFrequency = 0;      // Hz
+        auto framBlinkFrequency = 2;       // Hz
+        auto rfBlinkFrequency = 5;         // Hz
+        while(true)
         {
-            PRINTF("\nAll checks passed\n");
-            ledGpioPin.Set();
-        }
-        else
-        {
-            PRINTF("\nNot all checks passed\n");
+            if(flashIdsMatch)
+            {
+                BlinkLed(flashBlinkFrequency, blinkDuration);
+            }
+            if(framIdsMatch)
+            {
+                BlinkLed(framBlinkFrequency, blinkDuration);
+            }
+            if(rfIdsMatch)
+            {
+                BlinkLed(rfBlinkFrequency, blinkDuration);
+            }
         }
     }
 } deviceIdsTest;
@@ -121,5 +132,31 @@ auto PrintFramDeviceId(periphery::fram::DeviceId const & deviceId) -> void
     PRINTF("'");
     PRINTF("%02x", static_cast<unsigned int>(deviceId[1]));
     PRINTF("%02x", static_cast<unsigned int>(deviceId[0]));
+}
+
+
+// frequency is in Hz, duration in s
+auto BlinkLed(int frequency, int duration) -> void
+{
+    if(frequency < 0)
+    {
+        return;
+    }
+    if(frequency == 0)
+    {
+        ledGpioPin.Set();
+        AT(NOW() + duration * SECONDS);
+    }
+    else
+    {
+        auto ledIsOn = ledGpioPin.Read() == hal::PinState::set;
+        for(int i = 0; i < 2 * frequency * duration; ++i)
+        {
+            ledIsOn ? ledGpioPin.Reset() : ledGpioPin.Set();
+            ledIsOn = not ledIsOn;
+            AT(NOW() + SECONDS / frequency / 2LL);
+        }
+    }
+    ledGpioPin.Reset();
 }
 }
