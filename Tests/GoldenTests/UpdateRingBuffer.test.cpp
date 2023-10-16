770b44d5561c3b61548f1b09514aa45f1b706774
Jerome Hue <hue.jerome@yahoo.fr>
2023-10-16 20:55:02 +0200

    Improve UpdateRingBuffer.test.cpp
    Make it use the real UpdateProgramStatusHistory method by linking
    against Sts1CobcSw_Edu.

--- Tests/GoldenTests/UpdateRingBuffer.test.cpp
+++ Tests/GoldenTests/UpdateRingBuffer.test.cpp
@@ -1,4 +1,5 @@
 #include <Sts1CobcSw/Dummy.hpp>
+#include <Sts1CobcSw/Edu/ProgramStatusHistory.hpp>
 
 #include <ringbuffer.h>
 
@@ -14,93 +15,89 @@ std::uint32_t printfMask = 0;
 namespace sts1cobcsw
 {
 
-enum Status
-{
-    notStarted,
-    running,
-    done
-};
-
-struct Entry
-{
-    std::uint32_t id;
-    Status status;
-};
-
-auto StatusToString(Status status) -> std::string
+auto StatusToString(edu::ProgramStatus status) -> std::string
 {
     switch(status)
     {
-        case notStarted:
-            return {"notStarted"};
-        case running:
-            return {"running"};
-        case done:
-            return {"done"};
+        case edu::ProgramStatus::programRunning:
+            return {"programRunning"};
+        case edu::ProgramStatus::programExecutionFailed:
+            return {"programExecutionFailed"};
+        case edu::ProgramStatus::programExecutionSucceeded:
+            return {"programExecutionSucceeded"};
         default:
             break;
     }
 }
 
-// Defined a global variable for testing purpose
-RODOS::RingBuffer<Entry, 10> buffer;
 
-auto UpdateRingBuffer(Status status, std::uint32_t id, Status newStatus) -> void
-{
-    auto isLookedForEntry = [&](Entry & entry)
-    { return (entry.id == id and entry.status == status); };
-
-    auto count = 0;
-    for(int i = 0; i < buffer.getLen(); ++i)
-    {
-        if(buffer.vals[i].id == id and buffer.vals[i].status == status)
-        {
-            count++;
-        }
-    }
-
-    assert(count == 1 and "More than one program and status history match founds");
-
-    for(int i = 0; i < buffer.getLen(); ++i)
-    {
-        if(buffer.vals[i].id == id and buffer.vals[i].status == status)
-        {
-            buffer.vals[i].status = newStatus;
-        }
-    }
-}
-
-
+// Helper function for edu::programStatusHistory
 void PrintBuffer()
 {
-    for(int i = 0; i < buffer.occupiedCnt; ++i)
+    for(int i = 0; i < edu::programStatusHistory.occupiedCnt; ++i)
     {
         RODOS::PRINTF("Vals[%d] = .id(%d), .status(%s)\n",
                       i,
-                      buffer.vals[i].id,
-                      StatusToString(buffer.vals[i].status).c_str());
+                      edu::programStatusHistory.vals[i].programId.get(),
+                      StatusToString(edu::programStatusHistory.vals[i].status).c_str());
     }
 }
+
 
 class HelloDummy : public RODOS::StaticThread<>
 {
     void run() override
     {
         printfMask = 1;
-        buffer.put(Entry{.id = 1, .status = Status::notStarted});
-        buffer.put(Entry{.id = 2, .status = Status::notStarted});
-        buffer.put(Entry{.id = 3, .status = Status::notStarted});
-        buffer.put(Entry{.id = 4, .status = Status::notStarted});
-        buffer.put(Entry{.id = 5, .status = Status::notStarted});
+        edu::programStatusHistory.put(
+            edu::ProgramStatusHistoryEntry{.programId = static_cast<std::uint16_t>(1),
+                                           .queueId = static_cast<std::uint16_t>(1),
+                                           .status = edu::ProgramStatus::programExecutionFailed});
 
-        RODOS::PRINTF("Occupied count : %d\n", buffer.occupiedCnt);
+        edu::programStatusHistory.put(
+            edu::ProgramStatusHistoryEntry{.programId = static_cast<std::uint16_t>(2),
+                                           .queueId = static_cast<std::uint16_t>(1),
+                                           .status = edu::ProgramStatus::programRunning});
+
+        edu::programStatusHistory.put(
+            edu::ProgramStatusHistoryEntry{.programId = static_cast<std::uint16_t>(3),
+                                           .queueId = static_cast<std::uint16_t>(1),
+                                           .status = edu::ProgramStatus::programRunning});
+
+        edu::programStatusHistory.put(
+            edu::ProgramStatusHistoryEntry{.programId = static_cast<std::uint16_t>(4),
+                                           .queueId = static_cast<std::uint16_t>(1),
+                                           .status = edu::ProgramStatus::programRunning});
+
+
+        auto readCnt = edu::programStatusHistory.readCnt;
+        auto writeCnt = edu::programStatusHistory.writeCnt;
+        auto occupiedCnt = edu::programStatusHistory.occupiedCnt;
+
 
         // Print RingBuffer
         PrintBuffer();
 
-        // Start programs 1 , 3 and 5
-        UpdateRingBuffer(Status::notStarted, 1, Status::running);
-        UpdateRingBuffer(Status::notStarted, 3, Status::running);
+
+        edu::UpdateProgramStatusHistory(2, 1, edu::ProgramStatus::programExecutionSucceeded);
+        edu::UpdateProgramStatusHistory(4, 1, edu::ProgramStatus::programExecutionFailed);
+
+        edu::programStatusHistory.put(
+            edu::ProgramStatusHistoryEntry{.programId = static_cast<std::uint16_t>(5),
+                                           .queueId = static_cast<std::uint16_t>(1),
+                                           .status = edu::ProgramStatus::programRunning});
+
+        edu::UpdateProgramStatusHistory(5, 1, edu::ProgramStatus::programExecutionSucceeded);
+
+        // 1, we did not read anything
+        RODOS::PRINTF("readCnt unchanged     : %d\n",
+                      static_cast<int>(edu::programStatusHistory.readCnt == readCnt));
+        // 0, bc we did write
+        RODOS::PRINTF("writeCnt unchanged    : %d\n",
+                      static_cast<int>(edu::programStatusHistory.readCnt == writeCnt));
+        // 0
+        RODOS::PRINTF("OccupiedCnt unchanged : %d\n",
+                      static_cast<int>(edu::programStatusHistory.occupiedCnt == occupiedCnt));
 
         // Print RingBuffer
         PrintBuffer();
