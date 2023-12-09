abb62e26a4023bdedaba65b4292e7a0cd4a449d6
Patrick Kappl <kappl.pa@gmail.com>
2023-12-09 10:54:10 +0000

    Remove unused functions in EduMock.cpp

--- Sts1CobcSw/Edu/EduMock.cpp
+++ Sts1CobcSw/Edu/EduMock.cpp
@@ -10,14 +10,7 @@ using RODOS::PRINTF;
 using utility::PrintFormattedSystemUtc;
 
 
-// TODO: Move this to the proper file
-auto ResumeEduErrorCommunicationThread() -> void
-{
-    PRINTF("\nCall to ResumeEduErrorCommunicationThread()\n");
-    PrintFormattedSystemUtc();
-}
-
-
+// TODO: Move this to EduProgramQueueThreadMock.cpp or something
 auto ResumeEduProgramQueueThread() -> void
 {
     PRINTF("Call to ResumeEduProgramQueueThread()\n");
@@ -88,36 +81,6 @@ auto UpdateTime(UpdateTimeData const & data) -> Result<void>
     PrintFormattedSystemUtc();
     PRINTF("Call to UpdateTime(currentTime = %" PRIi32 ")\n", data.currentTime);
     return outcome_v2::success();
-}
-
-
-auto SendCommand(Byte commandId) -> void
-{
-    PrintFormattedSystemUtc();
-    PRINTF("Call to SendCommand(commandId = 0x%02x)\n", static_cast<unsigned int>(commandId));
-}
-
-
-auto SendData(std::span<Byte> data) -> Result<void>
-{
-    PrintFormattedSystemUtc();
-    PRINTF("Call to SendData(size(data) = %d)\n", size(data));
-    return outcome_v2::success();
-}
-
-
-auto UartReceive([[maybe_unused]] std::span<Byte> destination) -> Result<void>
-{
-    PrintFormattedSystemUtc();
-    PRINTF("Call to UartReceive(size(destination) = %d)\n", size(destination));
-    return outcome_v2::success();
-}
-
-
-void FlushUartBuffer()
-{
-    PrintFormattedSystemUtc();
-    PRINTF("Call to FlushUartBuffer()\n");
 }
 }
 }
