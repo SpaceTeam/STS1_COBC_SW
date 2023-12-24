cd5f322b367f9679055ffab7b29f955f0cba28bc
Patrick Kappl <kappl.pa@gmail.com>
2023-12-24 13:17:11 +0000

    Add test for UART functions with timeout
    I tried to test the non-blocking functions with the UCI UART but that
    did not work. We have to use the EDU UART. I guess that has to do with
    PRINTF().

--- Tests/HardwareTests/Uart.test.cpp
+++ Tests/HardwareTests/Uart.test.cpp
@@ -10,12 +10,14 @@
 #include <Sts1CobcSw/Hal/IoNames.hpp>
 #include <Sts1CobcSw/Hal/PinNames.hpp>
 #include <Sts1CobcSw/Hal/Uart.hpp>
+#include <Sts1CobcSw/Serial/Byte.hpp>
 #include <Sts1CobcSw/Utility/Span.hpp>
 
 #include <rodos_no_using_namespace.h>
 
 #include <array>
 #include <charconv>
+#include <cinttypes>
 #include <cstddef>
 #include <span>
 
@@ -49,20 +51,118 @@ class UartTest : public RODOS::StaticThread<>
 
     void run() override
     {
-        hal::WriteTo(&uciUart, Span("Hello UCI UART!\n"));
+        using RODOS::MICROSECONDS;
+        using RODOS::MILLISECONDS;
+        using RODOS::NOW;
+        using RODOS::PRINTF;
+
+        PRINTF("\n");
+        PRINTF("Is an EDU connected to test the non-blocking UART functions? (y/n)\n");
+        auto isEduConnected = std::array<Byte, 1>{};
+        hal::ReadFrom(&uciUart, Span(&isEduConnected));
+        if(isEduConnected[0] == static_cast<Byte>('y'))
+        {
+            // Update time command with timestamp = 0x00'00'00'00
+            auto message = std::to_array<Byte>({0x8B_b,
+                                                0x05_b,
+                                                0x00_b,
+                                                0x06_b,
+                                                0x00_b,
+                                                0x00_b,
+                                                0x00_b,
+                                                0x00_b,
+                                                0x05_b,
+                                                0xE7_b,
+                                                0xFE_b,
+                                                0xF5_b});
+            PRINTF("\n");
+            PRINTF("Sending a message containing an update time command to the EDU.\n");
+            PRINTF("message.size() = %i\n", static_cast<int>(message.size()));
+
+            auto tWrite0 = NOW();
+            hal::WriteTo(&eduUart, Span(message), 1000 * MILLISECONDS);
+            auto tWrite1 = NOW();
+
+            auto answer1 = std::array<char, 1>{};
+
+            auto tRead0 = NOW();
+            hal::ReadFrom(&eduUart, Span(&answer1), 1000 * MILLISECONDS);
+            auto tRead1 = NOW();
+
+            PRINTF("\n");
+            PRINTF("tWrite1 - tWrite0 = %8" PRIi64 " us\n", (tWrite1 - tWrite0) / MICROSECONDS);
+            PRINTF("tRead1  - tRead0  = %8" PRIi64 " us\n", (tRead1 - tRead0) / MICROSECONDS);
+            PRINTF("answer1 = 0x%02x\n", answer1[0]);
+
+            PRINTF("\n");
+            PRINTF("Sending the message again.");
+            auto timeout = 500 * MICROSECONDS;
+            PRINTF(" This time with a timeout of %" PRIi64 " us which is too short.\n",
+                   timeout / MICROSECONDS);
+
+            tWrite0 = NOW();
+            hal::WriteTo(&eduUart, Span(message), timeout);
+            tWrite1 = NOW();
+
+            answer1[0] = 0x00;
+
+            tRead0 = NOW();
+            hal::ReadFrom(&eduUart, Span(&answer1), 1000 * MILLISECONDS);
+            tRead1 = NOW();
+
+            PRINTF("\n");
+            PRINTF("tWrite1 - tWrite0 = %8" PRIi64 " us\n", (tWrite1 - tWrite0) / MICROSECONDS);
+            PRINTF("tRead1  - tRead0  = %8" PRIi64 " us\n", (tRead1 - tRead0) / MICROSECONDS);
+            PRINTF("answer1 = 0x%02x\n", answer1[0]);
+
+            PRINTF("\n");
+            PRINTF("Sending the message twice.\n");
+
+            tWrite0 = NOW();
+            hal::WriteTo(&eduUart, Span(message), 1000 * MILLISECONDS);
+            tWrite1 = NOW();
+            hal::WriteTo(&eduUart, Span(message), 1000 * MILLISECONDS);
+            auto tWrite2 = NOW();
+
+            auto answer2 = std::array<char, 1>{};
+
+            tRead0 = NOW();
+            hal::ReadFrom(&eduUart, Span(&answer1), 1000 * MILLISECONDS);
+            tRead1 = NOW();
+            hal::ReadFrom(&eduUart, Span(&answer2), 1000 * MILLISECONDS);
+            auto tRead2 = NOW();
+
+            PRINTF("\n");
+            PRINTF("tWrite1 - tWrite0 = %8" PRIi64 " us\n", (tWrite1 - tWrite0) / MICROSECONDS);
+            PRINTF("tWrite2 - tWrite1 = %8" PRIi64 " us\n", (tWrite2 - tWrite1) / MICROSECONDS);
+            PRINTF("tRead1  - tRead0  = %8" PRIi64 " us\n", (tRead1 - tRead0) / MICROSECONDS);
+            PRINTF("tRead2  - tRead1  = %8" PRIi64 " us\n", (tRead2 - tRead1) / MICROSECONDS);
+            PRINTF("answer1 = 0x%02x\n", answer1[0]);
+            PRINTF("answer2 = 0x%02x\n", answer2[0]);
+
+            PRINTF("\n");
+            PRINTF("Done with non-blocking EDU UART functions.\n");
+        }
+
+        PRINTF("\n");
 
         while(true)
         {
             constexpr auto bufferSize = 5;
             auto buffer = std::array<std::byte, bufferSize>{};
 
-            hal::WriteTo(&uciUart, Span("\nPlease send "));
+            hal::WriteTo(&uciUart, Span('\r'));
+            hal::WriteTo(&uciUart, Span('\n'));
+            hal::WriteTo(&uciUart, Span("Please send "));
             hal::WriteTo(&uciUart, Span(ToChars(bufferSize)));
-            hal::WriteTo(&uciUart, Span(" characters\n"));
+            hal::WriteTo(&uciUart, Span(" characters via UCI UART"));
+            hal::WriteTo(&uciUart, Span('\r'));
+            hal::WriteTo(&uciUart, Span('\n'));
             hal::ReadFrom(&uciUart, Span(&buffer));
             hal::WriteTo(&uciUart, Span("You sent: "));
             hal::WriteTo(&uciUart, Span(buffer));
-            hal::WriteTo(&uciUart, Span("\n"));
+            hal::WriteTo(&uciUart, Span('\r'));
+            hal::WriteTo(&uciUart, Span('\n'));
         }
     }
 } uartTest;
