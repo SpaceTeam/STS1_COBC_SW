2b5a66407e2db6ee60ef073093232e0168d45039
Daniel Schloms <daniel.schloms@spaceteam.at>
2024-01-14 21:47:08 +0100

    Address minor requested changes for HW CRC32 test

--- Tests/HardwareTests/HardwareCrc32.test.cpp
+++ Tests/HardwareTests/HardwareCrc32.test.cpp
@@ -1,53 +1,67 @@
 #include <Sts1CobcSw/Hal/GpioPin.hpp>
 #include <Sts1CobcSw/Hal/PinNames.hpp>
 #include <Sts1CobcSw/Periphery/HardwareCrc32.hpp>
+#include <Sts1CobcSw/Utility/Span.hpp>
+#include <Sts1CobcSw/Serial/Byte.hpp>
 
 #include <rodos_no_using_namespace.h>
 
+#include <array>
 #include <span>
-#include <string_view>
 
 namespace sts1cobcsw
 {
 auto test0Trailing =
-    std::array<uint8_t, 8>{0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3};  // word-aligned
+    std::to_array<Byte>{0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3};  // word-aligned
 auto test1Trailing =
-    std::array<uint8_t, 9>{0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3, 0xAB};  // 1 trailing
-auto test2Trailing = std::array<uint8_t, 10>{
+    std::to_array<Byte>{0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3, 0xAB};  // 1 trailing
+auto test2Trailing = std::to_array<Byte>{
     0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3, 0xAB, 0xFF};  // 2 trailing
-auto test3Trailing = std::array<uint8_t, 11>{
+auto test3Trailing = std::to_array<Byte>{
     0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3, 0xAB, 0xFF, 0x10};  // 3 trailing
+auto testWords = std::to_array<std::uint32_t>{0xDEADBEEF, 0xCABBA5E3};
 
 class HardwareCrc32Test : public RODOS::StaticThread<>
 {
+public:
+    HardwareCrc32Test() : StaticThread("HardwareCrc32Test")
+    {
+    }
+
+
+private:
     void init() override
     {
     }
 
     void run() override
     {
-        auto testDataWord = std::to_array<uint32_t>({0xDEADBEEF, 0xCABBA5E3});
-        // Online calc comparison:
-        // DEADBEEFCABBA5E3: 0xA962D97B
-        // DEADBEEFCABBA5E3000000AB: 0xBD5677DF
-        // DEADBEEFCABBA5E30000ABFF: 0x8C49583B
-        // DEADBEEFCABBA5E300ABFF10: 0xBE7357D8
-        // EFBEADDEE3A5BBCA: 0x78B4282B
+        // Online CRC32 (MPEG-2) comparison:
+        // DEADBEEFCABBA5E3:            0xA962D97B
+        // DEADBEEFCABBA5E3000000AB:    0xBD5677DF
+        // DEADBEEFCABBA5E30000ABFF:    0x8C49583B
+        // DEADBEEFCABBA5E300ABFF10:    0xBE7357D8
+        // EFBEADDEE3A5BBCA:            0x78B4282B
 
-        periphery::EnableHardwareCrcAndDma();
+        utility::InitializeCrc32Hardware();
         // Wait shortly for better readability in HTerm
-        RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
+
+        // Currently the DMA CRC32 is implemented in a blocking manner, so we can just test like this
+        // RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
         RODOS::PRINTF("DMA CRC32 0 Trailing:\n");
-        periphery::DmaCrc32(std::span<uint8_t>(test0Trailing));
-        RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
+        RODOS::PRINTF("CRC32: %u\n", utility::ComputeCrc32(utility::Span(test0Trailing)));
+
+        // RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
         RODOS::PRINTF("DMA CRC32 1 Trailing:\n");
-        periphery::DmaCrc32(std::span<uint8_t>(test1Trailing));
-        RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
+        RODOS::PRINTF("CRC32: %u\n", utility::ComputeCrc32(utility::Span(test1Trailing)));
+
+        // RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
         RODOS::PRINTF("DMA CRC32 2 Trailing:\n");
-        periphery::DmaCrc32(std::span<uint8_t>(test2Trailing));
-        RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
+        RODOS::PRINTF("CRC32: %u\n", utility::ComputeCrc32(utility::Span(test2Trailing)));
+
+        // RODOS::AT(RODOS::NOW() + 200 * RODOS::MILLISECONDS);
         RODOS::PRINTF("DMA CRC32 3 Trailing:\n");
-        periphery::DmaCrc32(std::span<uint8_t>(test3Trailing));
+        RODOS::PRINTF("CRC32: %u\n", utility::ComputeCrc32(utility::Span(test3Trailing)));
     }
 } hardwareCrc32Test;
 }
