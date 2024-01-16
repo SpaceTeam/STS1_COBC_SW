c8a2bccb50d1bd06278fd78edb4b1b9527591466
Daniel Schloms <daniel.schloms@spaceteam.at>
2024-01-16 02:00:37 +0100

    Refactor HW CRC32 test

--- Tests/HardwareTests/HardwareCrc32.test.cpp
+++ Tests/HardwareTests/HardwareCrc32.test.cpp
@@ -27,36 +27,17 @@
 #include <rodos_no_using_namespace.h>
 
 #include <array>
+#include <span>
 
 namespace sts1cobcsw
 {
-constexpr auto test0Trailing = std::to_array<Byte>(
-    {0xDE_b, 0xAD_b, 0xBE_b, 0xEF_b, 0xCA_b, 0xBB_b, 0xA5_b, 0xE3_b});  // word-aligned
-constexpr auto truth0Trailing = 0x78B4282BU;
+constexpr auto testByteData = std::to_array<Byte>(
+    {0xDE_b, 0xAD_b, 0xBE_b, 0xEF_b, 0xCA_b, 0xBB_b, 0xA5_b, 0xE3_b, 0xAB_b, 0xFF_b, 0x10_b});
+constexpr auto truthByteData =
+    std::to_array<uint32_t>({0x78B4282BU, 0x3B485F9B, 0x75F77B52, 0x687DB322});
 
-constexpr auto test1Trailing = std::to_array<Byte>(
-    {0xDE_b, 0xAD_b, 0xBE_b, 0xEF_b, 0xCA_b, 0xBB_b, 0xA5_b, 0xE3_b, 0xAB_b});  // 1 trailing
-constexpr auto truth1Trailing = 0x3B485F9B;
-
-constexpr auto test2Trailing = std::to_array<Byte>(
-    {0xDE_b, 0xAD_b, 0xBE_b, 0xEF_b, 0xCA_b, 0xBB_b, 0xA5_b, 0xE3_b, 0xAB_b, 0xFF_b});  // 2
-                                                                                        // trailing
-constexpr auto truth2Trailing = 0x75F77B52;
-
-constexpr auto test3Trailing = std::to_array<Byte>({0xDE_b,
-                                                    0xAD_b,
-                                                    0xBE_b,
-                                                    0xEF_b,
-                                                    0xCA_b,
-                                                    0xBB_b,
-                                                    0xA5_b,
-                                                    0xE3_b,
-                                                    0xAB_b,
-                                                    0xFF_b,
-                                                    0x10_b});  // 3 trailing
-constexpr auto truth3Trailing = 0x687DB322;
-
-auto testWords = std::to_array<std::uint32_t>({0xDEADBEEF, 0xCABBA5E3});
+auto testWordData = std::to_array<std::uint32_t>({0xDEADBEEF, 0xCABBA5E3});
+constexpr auto truthWordData = static_cast<uint32_t>(0xA962D97BU);
 
 class HardwareCrc32Test : public RODOS::StaticThread<>
 {
@@ -74,39 +55,42 @@ private:
     void run() override
     {
         utility::InitializeCrc32Hardware();
+        static_assert(testByteData.size() > sizeof(uint32_t));
+        auto nTrailingBytes = testByteData.size() % sizeof(uint32_t);
+        RODOS::PRINTF(
+            "INFO: Keep in mind that the DMA CRC32 version inverts endianness word-wise!\nSo "
+            "{0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3, 0xAB, 0xFF, 0x10}\n = {0xEF, 0xBE, "
+            "0xAD, 0xDE, 0xE3, 0xA5, 0xBB, 0xCA, 0x00, 0x10, 0xFF, 0xAB}\nwhen using a byte-based "
+            "CRC32 calculator.\n");
 
-        // Currently the DMA CRC32 is implemented in a blocking manner, so we can just test like
-        // NOLINTBEGIN(clang-diagnostic-format)
-        RODOS::PRINTF("\nDMA CRC32 0 Trailing:\n");
-        auto result0Trailing = utility::ComputeCrc32(Span(test0Trailing));
-        RODOS::PRINTF("CRC32: %lx, Reference: %x, %s\n",
-                      result0Trailing,
-                      truth0Trailing,
-                      result0Trailing == truth0Trailing ? "PASS" : "FAIL");
+        // Currently the DMA CRC32 is implemented in a blocking manner, so we can just test like this
+        for(size_t i = 0; i <= nTrailingBytes; i++)
+        {
+            // NOLINTNEXTLINE(clang-diagnostic-format)
+            RODOS::PRINTF("\nDMA CRC32 %u Trailing:\nData buffer:\n", i);
 
-        RODOS::PRINTF("\nDMA CRC32 1 Trailing:\n");
-        auto result1Trailing = utility::ComputeCrc32(Span(test1Trailing));
-        RODOS::PRINTF("CRC32: %lx, Reference: %x, %s\n",
-                      result1Trailing,
-                      truth1Trailing,
-                      result1Trailing == truth1Trailing ? "PASS" : "FAIL");
+            auto dataSpan = Span(testByteData).subspan(0, testByteData.size() - nTrailingBytes + i);
+            for(auto && element : dataSpan)
+            {
+                RODOS::PRINTF("%x ", static_cast<uint8_t>(element));
+            }
 
-        RODOS::PRINTF("\nDMA CRC32 2 Trailing:\n");
-        auto result2Trailing = utility::ComputeCrc32(Span(test2Trailing));
-        RODOS::PRINTF("CRC32: %lx, Reference: %x, %s\n",
-                      result2Trailing,
-                      truth2Trailing,
-                      result2Trailing == truth2Trailing ? "PASS" : "FAIL");
-
-        RODOS::PRINTF("\nDMA CRC32 3 Trailing:\n");
-        auto result3Trailing = utility::ComputeCrc32(Span(test3Trailing));
-        RODOS::PRINTF("CRC32: %lx, Reference: %x, %s\n",
-                      result3Trailing,
-                      truth3Trailing,
-                      result3Trailing == truth3Trailing ? "PASS" : "FAIL");
+            auto result = utility::ComputeCrc32(dataSpan);
+            // NOLINTBEGIN(clang-diagnostic-format)
+            RODOS::PRINTF("\nCRC32: %lx, Reference: %lx, %s\n",
+                          result,
+                          truthByteData[i],
+                          result == truthByteData[i] ? "PASS" : "FAIL");
+            // NOLINTEND(clang-diagnostic-format)
+        }
 
         RODOS::PRINTF("\nNon-DMA HW CRC32 (words) -> keeps endianness:\n");
-        RODOS::PRINTF("CRC32: %lx\n", utility::ComputeCrc32Blocking(Span(&testWords)));
+        auto wordDataResult = utility::ComputeCrc32Blocking(Span(&testWordData));
+        // NOLINTBEGIN(clang-diagnostic-format)
+        RODOS::PRINTF("CRC32: %lx, Reference: %lx, %s\n",
+                      wordDataResult,
+                      truthWordData,
+                      wordDataResult == truthWordData ? "PASS" : "FAIL");
         // NOLINTEND(clang-diagnostic-format)
     }
 } hardwareCrc32Test;
