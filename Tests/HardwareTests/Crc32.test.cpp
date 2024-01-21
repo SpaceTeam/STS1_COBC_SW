f6a32a973b13c1ceec9c3e9b953eac5e70f4c910
Patrick Kappl <kappl.pa@gmail.com>
2024-01-21 16:42:45 +0000

    Refactor Crc32.test.cpp and make output a bit nicer

--- Tests/HardwareTests/Crc32.test.cpp
+++ Tests/HardwareTests/Crc32.test.cpp
@@ -40,13 +40,13 @@ namespace sts1cobcsw
 using RODOS::PRINTF;
 
 
-auto const testByteData = std::to_array<Byte>(
+auto const byteData = std::to_array<Byte>(
     {0xDE_b, 0xAD_b, 0xBE_b, 0xEF_b, 0xCA_b, 0xBB_b, 0xA5_b, 0xE3_b, 0xAB_b, 0xFF_b, 0x10_b});
-auto const truthByteData =
+auto const correctCrc32ValuesOfBytes =
     std::to_array<std::uint32_t>({0x78B4282BU, 0x3B485F9B, 0x75F77B52, 0x687DB322});
 
-auto const testWordData = std::to_array<std::uint32_t>({0xDEADBEEF, 0xCABBA5E3});
-std::uint32_t const truthWordData = 0xA962D97B;
+auto const wordData = std::to_array<std::uint32_t>({0xDEADBEEF, 0xCABBA5E3});
+std::uint32_t const correctCrc32ValueOfWords = 0xA962D97B;
 
 
 class Crc32Test : public RODOS::StaticThread<>
@@ -68,8 +68,8 @@ private:
         PRINTF("\nCRC32 test\n\n");
 
         utility::InitializeCrc32Hardware();
-        static_assert(testByteData.size() > sizeof(std::uint32_t));
-        auto nTrailingBytes = testByteData.size() % sizeof(std::uint32_t);
+        static_assert(byteData.size() > sizeof(std::uint32_t));
+        auto nTrailingBytes = byteData.size() % sizeof(std::uint32_t);
         RODOS::PRINTF(
             "INFO: Keep in mind that the DMA CRC32 version inverts endianness word wise!\n"
             "So {0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xBB, 0xA5, 0xE3, 0xAB, 0xFF, 0x10}\n"
@@ -78,31 +78,34 @@ private:
 
         // Currently the DMA CRC32 is implemented in a blocking manner, so we can just test like
         // this
+        PRINTF("\n");
+        PRINTF("Test DMA CRC32 with\n");
         for(std::size_t i = 0; i <= nTrailingBytes; ++i)
         {
             PRINTF("\n");
-            PRINTF("Test DMA CRC32 with %d trailing bytes\n\n", static_cast<int>(i));
-            PRINTF("Data:");
-            auto data = Span(testByteData).first(testByteData.size() - nTrailingBytes + i);
+            PRINTF("%d trailing bytes\n", static_cast<int>(i));
+            PRINTF("Data: ");
+            auto data = Span(byteData).first(byteData.size() - nTrailingBytes + i);
             for(auto const & element : data)
             {
                 PRINTF(" 0x%02x", static_cast<std::uint8_t>(element));
             }
+            PRINTF("\n");
 
             auto result = utility::ComputeCrc32(data);
             PRINTF("CRC32: 0x%08x == 0x%08x\n",
                    static_cast<unsigned int>(result),
-                   static_cast<unsigned int>(truthByteData[i]));
-            Check(result == truthByteData[i]);
+                   static_cast<unsigned int>(correctCrc32ValuesOfBytes[i]));
+            Check(result == correctCrc32ValuesOfBytes[i]);
         }
 
         PRINTF("\n");
         PRINTF("Test non-DMA HW CRC32 (words) -> keeps endianness\n");
-        auto wordDataResult = utility::ComputeCrc32Blocking(Span(testWordData));
+        auto wordDataResult = utility::ComputeCrc32Blocking(Span(wordData));
         PRINTF("CRC32: 0x%08x == 0x%08x\n",
                static_cast<unsigned int>(wordDataResult),
-               static_cast<unsigned int>(truthWordData));
-        Check(wordDataResult == truthWordData);
+               static_cast<unsigned int>(correctCrc32ValueOfWords));
+        Check(wordDataResult == correctCrc32ValueOfWords);
     }
 } crc32Test;
 }
