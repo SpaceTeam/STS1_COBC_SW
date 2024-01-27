b6519e5865cac31284b2f52d15d90e271be48272
Patrick Kappl <kappl.pa@gmail.com>
2024-01-27 18:00:12 +0000

    Use SW CRC32 again
    The normal ComputeCrc32() function changed to using the HW unit. The HW
    and SW methods are not identical though, so I switched to
    ComputeCrc32Sw() to get the behavior that the EDU expects right now.

--- Sts1CobcSw/Edu/Edu.cpp
+++ Sts1CobcSw/Edu/Edu.cpp
@@ -324,7 +324,7 @@ auto SendDataPacket(std::span<Byte const> data) -> Result<void>
     }
     // Casting the size to uint16_t is safe since it is checked against maxDataLength
     auto length = Serialize(static_cast<std::uint16_t>(data.size()));
-    auto checksum = Serialize(utility::ComputeCrc32(data));
+    auto checksum = Serialize(utility::ComputeCrc32Sw(data));
 
     auto nNacks = 0;
     while(nNacks < maxNNackRetries)
@@ -438,7 +438,7 @@ auto Receive(std::span<Byte> data) -> Result<void>
 // TODO: A parameter pack of spans would be very convenient
 auto ReceiveAndCheckCrc32(std::span<Byte const> data) -> Result<void>
 {
-    auto computedCrc32 = utility::ComputeCrc32(data);
+    auto computedCrc32 = utility::ComputeCrc32Sw(data);
     OUTCOME_TRY(auto receivedCrc32, Receive<std::uint32_t>());
     if(computedCrc32 != receivedCrc32)
     {
