6c627f5f2bd528ecb5d503e832c768b86514cff9
Daniel Schloms <daniel.schloms@spaceteam.at>
2024-03-10 17:01:42 +0100

    Add sensor reading to EPS test

--- Tests/HardwareTests/Eps.test.cpp
+++ Tests/HardwareTests/Eps.test.cpp
@@ -1,4 +1,5 @@
 #include <Sts1CobcSw/Periphery/Eps.hpp>
+#include <Sts1CobcSw/Serial/Serial.hpp>
 
 #include <rodos_no_using_namespace.h>
 
@@ -25,13 +26,25 @@ private:
     void run() override
     {
         PRINTF("\nEPS test\n\n");
-
         eps::Initialize();
         PRINTF("EPS ADCs initialized\n");
-
         PRINTF("\n");
-
-        // Here comes the rest of the EPS test
+        constexpr auto nSensorValues = eps::nChannels * eps::nAdcs;
+        
+        TIME_LOOP(0, 2 * RODOS::SECONDS)
+        {
+            RODOS::PRINTF("Reading...\n");
+            auto sensorData = eps::Read();
+            void const * dataPointer = sensorData.data();
+            RODOS::PRINTF("Result:\n");
+            for(size_t i = 0; i < nSensorValues; i++)
+            {
+                std::uint16_t value = 0U;
+                dataPointer = DeserializeFrom<std::endian::big>(dataPointer, &value);
+                RODOS::PRINTF("%u", value);
+            }
+            RODOS::PRINTF("\n");
+        }
     }
 } epsTest;
 }
