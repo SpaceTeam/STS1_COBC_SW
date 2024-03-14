05fabccbfe61e9fb0774416d58001bdd96f537db
Kamil Chmela <chmelakamil@atlas.cz>
2024-03-14 19:12:49 +0000

    Add format script and targets for CMake files

--- cmake/format-cmake.cmake
+++ cmake/format-cmake.cmake
@@ -8,7 +8,7 @@ endmacro()
 
 default(FIX NO)
 
-set(flag --output-replacements-xml)
+set(flag "")
 set(args OUTPUT_VARIABLE output)
 if(FIX)
     set(flag -i)
@@ -17,41 +17,15 @@ endif()
 
 file(
     GLOB_RECURSE
-    clang_files
-    Sts1CobcSw/*.cpp
-    Sts1CobcSw/*.hpp
-    Sts1CobcSw/*.ipp
-    Tests/*.cpp
-    Tests/*.hpp
-    Tests/*.ipp
+    files
+    cmake/*.cmake
+    CMakeLists.txt
 )
-
-file(GLOB_RECURSE cmake_files cmake/*.cmake CMakeLists.txt)
-
 set(badly_formatted "")
 set(output "")
 string(LENGTH "${CMAKE_SOURCE_DIR}/" path_prefix_length)
 
-foreach(file IN LISTS clang_files)
-    execute_process(
-        COMMAND clang-format --style=file "${flag}" "${file}"
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-        RESULT_VARIABLE result ${args}
-    )
-    if(NOT result EQUAL "0")
-        message(FATAL_ERROR "'${file}': formatter returned with ${result}")
-    endif()
-    if(NOT FIX AND output MATCHES "\n<replacement offset")
-        string(SUBSTRING "${file}" "${path_prefix_length}" -1 relative_file)
-        list(APPEND badly_formatted "${relative_file}")
-    endif()
-    set(output "")
-endforeach()
-
-foreach(file IN LISTS cmake_files)
-    if(NOT FIX)
-        set(flag "")
-    endif()
+foreach(file IN LISTS files)
     execute_process(
         COMMAND cmake-format -c /.cmake-format.py "${flag}" "${file}"
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
