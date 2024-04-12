8b8b5c33dfe2c94ea369c0559dd37504a15dd870
Patrick Kappl <kappl.pa@gmail.com>
2024-04-12 17:09:04 +0000

    Make FlatArray() also accept spans

--- Sts1CobcSw/Utility/FlatArray.hpp
+++ Sts1CobcSw/Utility/FlatArray.hpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <cstddef>
+#include <span>
 #include <type_traits>
 
 
@@ -15,12 +16,29 @@ inline constexpr std::size_t flatArraySize<std::array<T, size>> = size;
 template<typename T, std::size_t size>
 inline constexpr std::size_t flatArraySize<const std::array<T, size>> = size;
 
+template<typename T, std::size_t extent>
+    requires(extent != std::dynamic_extent)
+inline constexpr std::size_t flatArraySize<std::span<T, extent>> = extent;
 
-//! @brief  Create a flat `std::array` from the given arrays and/or values.
+template<typename T, std::size_t extent>
+    requires(extent != std::dynamic_extent)
+inline constexpr std::size_t flatArraySize<std::span<T const, extent>> = extent;
+
+// Not sure if we need the following two overloads as well, but better safe than sorry
+template<typename T, std::size_t extent>
+    requires(extent != std::dynamic_extent)
+inline constexpr std::size_t flatArraySize<const std::span<T, extent>> = extent;
+
+template<typename T, std::size_t extent>
+    requires(extent != std::dynamic_extent)
+inline constexpr std::size_t flatArraySize<const std::span<T const, extent>> = extent;
+
+
+//! @brief  Create a flat `std::array` from the given arrays, spans, and values.
 //!
-//! All arguments must be of the same type `T` or `std::array<T, size>`, although the sizes of the
-//! arrays can differ. The order of the elements in the resulting array is the same as the order of
-//! the arguments.
+//! All arguments must be of the same type `T`, `std::array<T, size>`, or `std::span<T, extent>`.
+//! The sizes/extents of the arrays/spans can differ, but the spans must not have dynamic extent.
+//! The order of the elements in the resulting array is the same as the order of the arguments.
 //!
 //! The return type `std::array<T, size>` is not explicitly specified because computing `T` and
 //! `size` from the arguments is not trivial.
