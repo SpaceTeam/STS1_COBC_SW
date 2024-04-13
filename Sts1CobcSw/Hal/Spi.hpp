b9b45df32a7f32ad5988ddd09b932fa6e36a62ea
Kamil Chmela <chmelakamil@atlas.cz>
2024-04-13 16:57:06 +0000

    Add spi class to supervise data transfer

--- Sts1CobcSw/Hal/Spi.hpp
+++ Sts1CobcSw/Hal/Spi.hpp
@@ -10,16 +10,28 @@
 
 namespace sts1cobcsw::hal
 {
-auto Initialize(RODOS::HAL_SPI * spi, std::uint32_t baudRate) -> void;
+class Spi
+{
+private:
+    RODOS::HAL_SPI spi;
+    RODOS::CommBuffer<std::int64_t> transferEnd;
 
+public:
+    friend auto Initialize(Spi * spiClass, std::int32_t baudRate) -> void;
+
+    template<typename T, std::size_t extent>
+    friend auto ReadFrom(Spi * spiClass, std::span<T const, extent> data, std::int64_t timeout)
+        -> void;
+
+    template<typename T, std::size_t extent>
+    friend auto WriteTo(Spi * spiClass, std::span<T, extent> data, std::int64_t timeout) -> void;
+
+    auto TransferEnd();
+};
+
+}
 // TODO: Maybe remove extent to reduce code bloat, or probably build time since it is just a single
 // call to the Rodos function, which is trivial to inline.
-template<typename T, std::size_t extent>
-auto WriteTo(RODOS::HAL_SPI * spi, std::span<T const, extent> data) -> void;
-
-template<typename T, std::size_t extent>
-auto ReadFrom(RODOS::HAL_SPI * spi, std::span<T, extent> data) -> void;
-}
 
 
 #include <Sts1CobcSw/Hal/Spi.ipp>  // IWYU pragma: keep
