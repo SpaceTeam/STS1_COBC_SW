00f12fd2b8a532480d1a9484b6e419b9e2e9357b
Kamil Chmela <chmelakamil@atlas.cz>
2024-04-13 16:57:06 +0000

    Add spi class to supervise data transfer

--- Sts1CobcSw/Hal/Spi.cpp
+++ Sts1CobcSw/Hal/Spi.cpp
@@ -2,13 +2,18 @@
 
 #include <rodos_no_using_namespace.h>
 
-
 namespace sts1cobcsw::hal
 {
-auto Initialize(RODOS::HAL_SPI * spi, std::uint32_t baudRate) -> void
+auto Initialize(Spi* spiClass, std::int32_t baudRate) -> void
 {
     // spi.init() only returns -1 if the SPI_IDX is out of range. Since we can check that statically
     // we do not need to report that error at runtime.
-    spi->init(baudRate, /*slave=*/false, /*tiMode=*/false);
+    spiClass->spi.init(baudRate, /*slave=*/false, /*tiMode=*/false);
+    spiClass->transferEnd.put(RODOS::END_OF_TIME);
+}
+
+auto Spi::TransferEnd()
+{
+    return this->transferEnd;
 }
 }
