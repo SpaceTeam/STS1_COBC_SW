b4da95733b38b1441f233537a49906c053c599a2
Kamil Chmela <chmelakamil@atlas.cz>
2024-04-13 17:31:18 +0000

    Add original functions

--- Sts1CobcSw/Hal/Spi.hpp
+++ Sts1CobcSw/Hal/Spi.hpp
@@ -29,6 +29,16 @@ public:
     auto TransferEnd();
 };
 
+auto Initialize(RODOS::HAL_SPI * spi, std::uint32_t baudRate) -> void;
+
+// TODO: Maybe remove extent to reduce code bloat, or probably build time since it is just a single
+// call to the Rodos function, which is trivial to inline.
+template<typename T, std::size_t extent>
+auto WriteTo(RODOS::HAL_SPI * spi, std::span<T const, extent> data) -> void;
+
+template<typename T, std::size_t extent>
+auto ReadFrom(RODOS::HAL_SPI * spi, std::span<T, extent> data) -> void;
+
 }
 // TODO: Maybe remove extent to reduce code bloat, or probably build time since it is just a single
 // call to the Rodos function, which is trivial to inline.
