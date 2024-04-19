8eff2b97da10be59901f075dc5516a810aa6858a
Kamil Chmela <chmelakamil@atlas.cz>
2024-04-19 11:46:54 +0000

    Fix formatting and const

--- Sts1CobcSw/Hal/Spi.cpp
+++ Sts1CobcSw/Hal/Spi.cpp
@@ -2,9 +2,10 @@
 
 #include <rodos_no_using_namespace.h>
 
+
 namespace sts1cobcsw::hal
 {
-auto Initialize(Spi * spiClass, std::int32_t baudRate) -> void
+auto Initialize(Spi * spiClass, std::uint32_t baudRate) -> void
 {
     // spi.init() only returns -1 if the SPI_IDX is out of range. Since we can check that statically
     // we do not need to report that error at runtime.
@@ -12,10 +13,12 @@ auto Initialize(Spi * spiClass, std::int32_t baudRate) -> void
     spiClass->transferEnd.put(RODOS::END_OF_TIME);
 }
 
-auto Spi::TransferEnd()
+
+auto Spi::TransferEnd() const
 {
     return this->transferEnd;
 }
+
 
 auto Initialize(RODOS::HAL_SPI * spi, std::uint32_t baudRate) -> void
 {
