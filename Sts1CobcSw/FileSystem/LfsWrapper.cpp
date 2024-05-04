4997cfce52c628d572137260f744fe462004f1f2
Florian Donauer <florian@donauer.at>
2024-05-04 19:47:16 +0000

    Add file class

--- Sts1CobcSw/FileSystem/LfsWrapper.cpp
+++ Sts1CobcSw/FileSystem/LfsWrapper.cpp
@@ -9,6 +9,67 @@ namespace sts1cobcsw::fs
 {
 auto lfs = lfs_t{};
 
+auto open(char const * path, int flag) -> Result<File>
+{
+    lfs_file_t lfsFile;
+    auto error = lfs_file_open(&lfs, &lfsFile, path, flag);
+    if(error == 0)
+    {
+        return File(lfsFile);
+    }
+    return static_cast<ErrorCode>(error);
+}
+
+File::~File()
+{
+    (void)close();
+}
+
+File::File(lfs_file_t & lfsFile) : m_lfsFile(lfsFile)
+{
+}
+
+template<typename T>
+auto File::read(T * t) -> Result<int>
+{
+    auto value = lfs_file_read(&lfs, &m_lfsFile, t, sizeof(T));
+    if(value >= 0)
+    {
+        return value;
+    }
+    return static_cast<ErrorCode>(value);
+}
+
+template<typename T>
+auto File::write(T const & t) -> Result<int>
+{
+    auto value = lfs_file_write(&lfs, &m_lfsFile, &t, sizeof(T));
+    if(value >= 0)
+    {
+        return value;
+    }
+    return static_cast<ErrorCode>(value);
+}
+
+auto File::FileSize() -> Result<int>
+{
+    auto value = lfs_file_size(&lfs, &m_lfsFile);
+    if(value >= 0)
+    {
+        return value;
+    }
+    return static_cast<ErrorCode>(value);
+}
+
+[[nodiscard]] auto File::close() -> Result<void>
+{
+    auto error = lfs_file_close(&lfs, &m_lfsFile);
+    if(error == 0)
+    {
+        return outcome_v2::success();
+    }
+    return static_cast<ErrorCode>(error);
+}
 
 [[nodiscard]] auto Mount() -> Result<void>
 {
--- Sts1CobcSw/FileSystem/LfsWrapper.hpp
+++ Sts1CobcSw/FileSystem/LfsWrapper.hpp
@@ -6,5 +6,32 @@
 
 namespace sts1cobcsw::fs
 {
+
 [[nodiscard]] auto Mount() -> Result<void>;
+
+class File
+{
+public:
+    // only allow creation of File class throu open friend function
+    File() = delete;
+    File(File const &) = delete;
+    File(File &&) = default;
+
+    ~File();
+
+    [[nodiscard]] auto close() -> Result<void>;
+    template<typename T>
+    auto read(T * t) -> Result<int>;
+    template<typename T>
+    auto write(T const & t) -> Result<int>;
+    auto FileSize() -> Result<int>;
+
+    friend auto open(char const * path, int flag) -> Result<File>;
+
+private:
+    File(lfs_file_t & lfsFile);
+
+    lfs_file_t & m_lfsFile;
+};
+
 }
