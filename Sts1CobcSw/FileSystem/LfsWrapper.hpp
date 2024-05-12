0281aff240f1d0e1265598d2f6399105c5b3db45
Patrick Kappl <kappl.pa@gmail.com>
2024-05-12 16:12:35 +0000

    Refactor LfsWrapper a bit

--- Sts1CobcSw/FileSystem/LfsWrapper.hpp
+++ Sts1CobcSw/FileSystem/LfsWrapper.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+
 #include <Sts1CobcSw/FileSystem/ErrorsAndResult.hpp>
 
 #include <littlefs/lfs.h>
@@ -13,31 +14,29 @@ class File;
 
 
 [[nodiscard]] auto Mount() -> Result<void>;
-
-
 [[nodiscard]] auto Open(std::string_view path, int flag) -> Result<File>;
 
 
 class File
 {
 public:
-    // only allow creation of File class throu Open friend function
     File(File const &) = delete;
     File(File &&) = default;
     auto operator=(File const &) -> File & = delete;
-    auto operator=(File &&) -> File & = delete;
+    auto operator=(File &&) -> File & = default;
     ~File();
 
-    [[nodiscard]] auto Close() -> Result<void>;
     template<typename T>
     [[nodiscard]] auto Read(T * t) -> Result<int>;
     template<typename T>
     [[nodiscard]] auto Write(T const & t) -> Result<int>;
     [[nodiscard]] auto Size() -> Result<int>;
+    [[nodiscard]] auto Close() -> Result<void>;
 
     friend auto Open(std::string_view path, int flag) -> Result<File>;
 
 private:
+    // Only allow creation of File class through friend function Open()
     File() = default;
 
     lfs_file_t lfsFile_;
