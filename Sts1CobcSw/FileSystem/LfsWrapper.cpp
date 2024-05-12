78262a9ca3e6c196e4ecc51970b786b6c1276943
Patrick Kappl <kappl.pa@gmail.com>
2024-05-12 16:12:35 +0000

    Refactor LfsWrapper a bit

--- Sts1CobcSw/FileSystem/LfsWrapper.cpp
+++ Sts1CobcSw/FileSystem/LfsWrapper.cpp
@@ -10,6 +10,17 @@ namespace sts1cobcsw::fs
 auto lfs = lfs_t{};
 
 
+[[nodiscard]] auto Mount() -> Result<void>
+{
+    auto error = lfs_mount(&lfs, &lfsConfig);
+    if(error == 0)
+    {
+        return outcome_v2::success();
+    }
+    return static_cast<ErrorCode>(error);
+}
+
+
 auto Open(std::string_view path, int flag) -> Result<File>
 {
     File file = File();
@@ -31,52 +42,41 @@ File::~File()
 template<typename T>
 auto File::Read(T * t) -> Result<int>
 {
-    auto value = lfs_file_read(&lfs, &lfsFile_, t, sizeof(T));
-    if(value >= 0)
+    auto nReadBytes = lfs_file_read(&lfs, &lfsFile_, t, sizeof(T));
+    if(nReadBytes >= 0)
     {
-        return value;
+        return nReadBytes;
     }
-    return static_cast<ErrorCode>(value);
+    return static_cast<ErrorCode>(nReadBytes);
 }
 
 
 template<typename T>
 auto File::Write(T const & t) -> Result<int>
 {
-    auto value = lfs_file_write(&lfs, &lfsFile_, &t, sizeof(T));
-    if(value >= 0)
+    auto nWrittenBytes = lfs_file_write(&lfs, &lfsFile_, &t, sizeof(T));
+    if(nWrittenBytes >= 0)
     {
-        return value;
+        return nWrittenBytes;
     }
-    return static_cast<ErrorCode>(value);
+    return static_cast<ErrorCode>(nWrittenBytes);
 }
 
 
 auto File::Size() -> Result<int>
 {
-    auto value = lfs_file_size(&lfs, &lfsFile_);
-    if(value >= 0)
+    auto size = lfs_file_size(&lfs, &lfsFile_);
+    if(size >= 0)
     {
-        return value;
+        return size;
     }
-    return static_cast<ErrorCode>(value);
+    return static_cast<ErrorCode>(size);
 }
 
 
 [[nodiscard]] auto File::Close() -> Result<void>
 {
     auto error = lfs_file_close(&lfs, &lfsFile_);
-    if(error == 0)
-    {
-        return outcome_v2::success();
-    }
-    return static_cast<ErrorCode>(error);
-}
-
-
-[[nodiscard]] auto Mount() -> Result<void>
-{
-    auto error = lfs_mount(&lfs, &lfsConfig);
     if(error == 0)
     {
         return outcome_v2::success();
