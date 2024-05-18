67fa2295cc48c9b08b3632972150251b04c92333
Daniel Schloms <daniel.schloms@gmail.com>
2024-05-18 17:02:58 +0200

    Remove comments copied from RODOS semaphore

--- Sts1CobcSw/Utility/LinuxSemaphore.cpp
+++ Sts1CobcSw/Utility/LinuxSemaphore.cpp
@@ -17,15 +17,12 @@ void LinuxSemaphore::Enter()
     int32_t callerPriority = caller->getPriority();
     {
         RODOS::PRIORITY_CEILER_IN_SCOPE();
-        // Check if semaphore is occupied by another thread
         if((owner != 0) && (owner != caller))
         {
-            // Avoid priority inversion
             if(callerPriority > owner->getPriority())
             {
                 owner->setPriority(callerPriority);
             }
-            // Sleep until wake up by leave
             while(owner != 0 && owner != caller)
             {
                 RODOS::Thread::suspendCallerUntil(RODOS::END_OF_TIME, this);
@@ -35,8 +32,8 @@ void LinuxSemaphore::Enter()
         owner = caller;
         ownerPriority = callerPriority;
         ownerEnterCnt++;
-    }                 // end of prio_ceiling
-    caller->yield();  // wating with prio_ceiling, maybe some one more important wants to work?
+    }
+    caller->yield();
 }
 
 
@@ -46,13 +43,13 @@ void LinuxSemaphore::Leave()
     RODOS::Thread * waiter = 0;
 
     if(owner != caller)
-    {  // User Programm error: What to do? Nothing!
+    {
         return;
     }
 
     ownerEnterCnt--;
     if(ownerEnterCnt > 0)
-    {  // same thread made multiple enter()
+    {
         return;
     }
 
@@ -63,29 +60,14 @@ void LinuxSemaphore::Leave()
 
         if(waiter != 0)
         {
-            owner = waiter;  // set new owner, so that no other thread can grep the semaphore before
-                             // thread switch
+            owner = waiter;
             waiter->resume();
         }
-    }  // end of PRIORITY_CEILER,
+    }
 
-    //   priority of current thread might have been increased in enter() due to a semaphore access
-    //   of another thread with higher priority
-    //   If so, restore the original priority
     RODOS::Thread::setPrioCurrentRunner(ownerPriority);
     ownerPriority = 0;
 
-    /*
-     * In case resume performs no yield:
-     * To speed up semaphore handling the waiter-thread should be executed asap
-     * -> that can be achieved by pause caller-thread with yield()
-     * If there is a scheduler-call (triggered by timer) before the yield-call, it is not
-     * necessary any more!
-     * -> to avoid unnecessary yield the variable ownerPriority will be checked
-     * -> if ownerPriority is still 0 there was no thread-switch before
-     * -> Of course there can be a thread switch directly before the yield-call. In this case
-     *    we have to accept the delay caused by the unnecessary yield-call.
-     */
     if((waiter != 0) && (ownerPriority == 0))
     {
         caller->yield();
@@ -99,7 +81,6 @@ auto LinuxSemaphore::TryEnter() -> bool
     int32_t callerPriority = caller->getPriority();
     {
         RODOS::PRIORITY_CEILER_IN_SCOPE();
-        // Check if semaphore is occupied by another thread
         if((owner != 0) && (owner != caller))
         {
             return false;
@@ -107,8 +88,8 @@ auto LinuxSemaphore::TryEnter() -> bool
         owner = caller;
         ownerPriority = callerPriority;
         ownerEnterCnt++;
-    }                 // end of prio_ceiling
-    caller->yield();  // wating with prio_ceiling, maybe some one more important wants to work?
+    }
+    caller->yield();
     return true;
 }
 }
