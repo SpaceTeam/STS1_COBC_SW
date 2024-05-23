6dc01870eb58915df9a40d5cac5be9229a131f71
Patrick Kappl <kappl.pa@gmail.com>
2024-05-23 19:09:45 +0000

    Define serialSize<ProgramId>

--- Sts1CobcSw/ProgramId/ProgramId.hpp
+++ Sts1CobcSw/ProgramId/ProgramId.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 
+#include <Sts1CobcSw/Serial/Serial.hpp>
+
 #include <NamedType/named_type.hpp>
 
 #include <cstdint>
@@ -9,4 +11,8 @@
 namespace sts1cobcsw
 {
 using ProgramId = fluent::NamedType<std::uint16_t, struct ProgramIdTag, fluent::Callable>;
+
+// TODO: Maybe make ProgramId completely serializable, i.e., overload (De-)Serialize() as well
+template<>
+inline constexpr std::size_t serialSize<ProgramId> = totalSerialSize<ProgramId::UnderlyingType>;
 }
