d24c5b0e697ca94d214a8b9661080c601e6da3d3
Florian Donauer <florian@donauer.at>
2024-06-03 17:02:47 +0000

    Add threads for power test

---
+++ Tests/HardwareTests/Power.test.cpp
@@ -0,0 +1,167 @@
+#include <Sts1CobcSw/Hal/GpioPin.hpp>
+#include <Sts1CobcSw/Hal/IoNames.hpp>
+#include <Sts1CobcSw/Hal/Uart.hpp>
+#include <Sts1CobcSw/Serial/Byte.hpp>
+#include <Sts1CobcSw/Utility/Span.hpp>
+
+#include <rodos/support/support-libs/random.h>
+#include <rodos_no_using_namespace.h>
+
+#include <array>
+#include <cstdint>
+
+
+namespace sts1cobcsw
+{
+using RODOS::PRINTF;
+auto pinsToTest = std::to_array<hal::GpioPin>({hal::led1Pin, hal::led2Pin});
+auto eduUart = RODOS::HAL_UART(hal::eduUartIndex, hal::eduUartTxPin, hal::eduUartRxPin);
+auto uciUart = RODOS::HAL_UART(hal::uciUartIndex, hal::uciUartTxPin, hal::uciUartRxPin);
+
+
+class PowerTestLed : public RODOS::StaticThread<>
+{
+public:
+    PowerTestLed() : StaticThread("PowerTestLed")
+    {
+    }
+
+
+private:
+    void init() override
+    {
+        for(auto & pin : pinsToTest)
+        {
+            pin.Direction(hal::PinDirection::out);
+        }
+    }
+
+
+    void run() override
+    {
+        PRINTF("\nPOWER test LED thread\n\n");
+        auto toggle = true;
+
+        TIME_LOOP(0, 1000 * RODOS::MILLISECONDS)
+        {
+            for(auto & pin : pinsToTest)
+            {
+                toggle ? pin.Set() : pin.Reset();
+            }
+            toggle = not toggle;
+        }
+    }
+};
+
+
+class PowerTestUart : public RODOS::StaticThread<>
+{
+public:
+    PowerTestUart() : StaticThread("PowerTestUart")
+    {
+    }
+
+
+private:
+    void init() override
+    {
+        RODOS::setRandSeed(static_cast<std::uint64_t>(RODOS::NOW()));
+        auto uartBaudRate = 115'200U;
+        hal::Initialize(&eduUart, uartBaudRate);
+    }
+
+
+    void run() override
+    {
+        PRINTF("\nPOWER test UART thread\n\n");
+
+        auto message = std::to_array<Byte>({0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b,
+                                            0x00_b});
+
+        while(true)
+        {
+            // generate random 12byte message
+            // ToDo fill message
+
+            PRINTF("Send random message to edu over UART\n");
+            hal::WriteTo(&eduUart, Span(message));  // use non blocking call
+        }
+    }
+};
+
+
+class PowerTestCustome : public RODOS::StaticThread<>
+{
+public:
+    PowerTestCustome() : StaticThread("PowerTestCustome")
+    {
+    }
+
+
+private:
+    void init() override
+    {
+        auto const baudRate = 115'200;
+        hal::Initialize(&uciUart, baudRate);
+    }
+
+
+    void run() override
+    {
+        PRINTF("\nPOWER test custome thread\n\n");
+        PRINTF("Select operation to perform:\n");
+        PRINTF("[1 - SPI Com]\n");
+        PRINTF("[2 - integer  calculation]\n");
+        PRINTF("[3 - floating calculation]\n");
+        PRINTF("[4 - ADC]\n");
+
+        auto command = std::array<Byte, 1>{};
+        hal::ReadFrom(&uciUart, Span(&command));
+        PRINTF("\n");
+        switch(static_cast<char>(command[0]))
+        {
+            case '1':
+            {
+                // ToDo start SPI
+                break;
+            }
+            case '2':
+            {
+                // ToDo start SPI
+                break;
+            }
+            case '3':
+            {
+                // ToDo start SPI
+                break;
+            }
+            case '4':
+            {
+                // ToDo add ADC
+                PRINTF("ADC not supported!\n");
+                break;
+            }
+            default:
+            {
+                PRINTF("Unknown command\n");
+                break;
+            }
+        }
+    }
+};
+
+
+static PowerTestLed powerTestLed;
+static PowerTestUart powerTestUart;
+static PowerTestCustome powerTestCustome;
+}
