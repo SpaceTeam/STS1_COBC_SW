a713fbe8e4c45c037e3e9d56d870223623cb56c2
Patrick Kappl <patrick.kappl@spaceteam.at>
2024-06-27 12:59:41 +0000

    Use void pointers for low-level RF functions

--- Sts1CobcSw/Periphery/Fram.hpp
+++ Sts1CobcSw/Periphery/Fram.hpp
@@ -27,6 +27,7 @@ auto Initialize() -> void;
 [[nodiscard]] auto ReadDeviceId() -> DeviceId;
 auto ActualBaudRate() -> std::int32_t;
 
+// TODO: This should also work without extent and thereby without any template parameters
 template<std::size_t extent>
 auto WriteTo(Address address, std::span<Byte const, extent> data, std::int64_t timeout) -> void;
 
