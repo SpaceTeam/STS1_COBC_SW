ee7ebe01954b27625c0b8df87ad42d0fdd09a770
Patrick Kappl <kappl.pa@gmail.com>
2024-07-02 20:14:01 +0000

    Remove all DEBUG_PRINT()s from rf::Send() and rf::WriteToFifo()

--- Sts1CobcSw/Periphery/Rf.cpp
+++ Sts1CobcSw/Periphery/Rf.cpp
@@ -196,7 +196,6 @@ auto SetTxType(TxType txType) -> void
 auto Send(void const * data, std::size_t nBytes) -> void
 {
     // TODO: Acc. the datasheet "fifo hardware does not need to be reset prior to use".
-    DEBUG_PRINT("ClearFifos()\n");
     ClearFifos();
 
     // Set TX Data Length
@@ -205,7 +204,6 @@ auto Send(void const * data, std::size_t nBytes) -> void
     static constexpr auto iPktField1Length = 0x0D_b;
     auto lengthUpperBits = static_cast<Byte>(nBytes >> CHAR_BIT);
     auto lengthLowerBits = static_cast<Byte>(nBytes);
-    DEBUG_PRINT("SetProperties(iPktField1Length)\n");
     SetProperties(PropertyGroup::pkt, iPktField1Length, Span({lengthUpperBits, lengthLowerBits}));
 
     // Fill the TX FIFO with 60 bytes each "round"
@@ -218,7 +216,6 @@ auto Send(void const * data, std::size_t nBytes) -> void
     // afterwards for the packet sent interrupt
     auto dataIndex = 0U;
     auto dataSpan = std::span(static_cast<Byte const *>(data), nBytes);
-    DEBUG_PRINT("while(nBytes - dataIndex > nFillBytes)\n");
     while(nBytes - dataIndex > nFillBytes)
     {
         // Enable the almost empty interrupt in the first round
@@ -226,22 +223,17 @@ auto Send(void const * data, std::size_t nBytes) -> void
         {
             // TODO: Setting interrupts could be put in a function
             static constexpr auto txFifoAlmostEmptyInterrupt = 0b0000'0010_b;
-            DEBUG_PRINT("Enable almostEmptyInterrupt\n");
             SetProperties(PropertyGroup::intCtl, iIntCtlPhEnable, Span(txFifoAlmostEmptyInterrupt));
             almostEmptyInterruptEnabled = true;
         }
 
         // Write nFillBytes bytes to the TX FIFO
         // NOLINTNEXTLINE(*pointer-arithmetic)
-        DEBUG_PRINT("WriteToFifo()\n");
         WriteToFifo(dataSpan.subspan(dataIndex, nFillBytes));
         dataIndex += nFillBytes;
-        DEBUG_PRINT("ClearInterrupts()\n");
         ClearInterrupts();
-        DEBUG_PRINT("StartTx()\n");
         StartTx();
         // Wait for TX FIFO almost empty interrupt
-        DEBUG_PRINT("Wait for TX FIFO almost empty interrupt\n");
         while(nirqGpioPin.Read() == hal::PinState::set)
         {
             // TODO: Add timeout
@@ -251,23 +243,18 @@ auto Send(void const * data, std::size_t nBytes) -> void
 
     // Enable packet sent interrupt
     static constexpr auto packetSentInterrupt = 0b0010'0000_b;
-    DEBUG_PRINT("Enable packet sent interrupt\n");
     SetProperties(PropertyGroup::intCtl, iIntCtlPhEnable, Span(packetSentInterrupt));
 
-    DEBUG_PRINT("ClearInterrupts()\n");
     ClearInterrupts();
 
     // Write the rest of the data
     // NOLINTNEXTLINE(*pointer-arithmetic)
-    DEBUG_PRINT("WriteToFifo(rest of data)\n");
     WriteToFifo(dataSpan.subspan(dataIndex));
-    DEBUG_PRINT("StartTx()\n");
     StartTx();
 
     auto startTime = RODOS::NOW();
 
     // Wait for Packet Sent Interrupt
-    DEBUG_PRINT("Wait for Packet Sent Interrupt()\n");
     while(nirqGpioPin.Read() == hal::PinState::set)
     {
         // TODO: Set timeout constant
@@ -277,7 +264,6 @@ auto Send(void const * data, std::size_t nBytes) -> void
         }
         RODOS::AT(RODOS::NOW() + 10 * RODOS::MICROSECONDS);
     }
-    DEBUG_PRINT("EnterStandby()\n");
     EnterStandby();
 }
 
@@ -1053,17 +1039,12 @@ auto EnterStandby() -> void
 // TODO: Refactor (issue #226)
 auto WriteToFifo(std::span<Byte const> data) -> void
 {
-    DEBUG_PRINT("csGpioPin.Reset()\n");
     csGpioPin.Reset();
     AT(NOW() + 20 * MICROSECONDS);
-    DEBUG_PRINT("WriteTo(&spi, Span(cmdWriteTxFifo), %lld);\n", spiTimeout);
     WriteTo(&spi, Span(cmdWriteTxFifo), spiTimeout);
-    DEBUG_PRINT("WriteTo(&spi, data, %lld);\n", spiTimeout);
     WriteTo(&spi, data, spiTimeout);
     AT(NOW() + 2 * MICROSECONDS);
-    DEBUG_PRINT("csGpioPin.Set();\n");
     csGpioPin.Set();
-    DEBUG_PRINT("WaitForCts();\n");
     WaitForCts();
     csGpioPin.Set();
 }
