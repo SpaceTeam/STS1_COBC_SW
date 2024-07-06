452c8bba1cb5238f081ac31de4f1a9d7e3345ef9
Patrick Kappl <kappl.pa@gmail.com>
2024-07-06 13:02:25 +0000

    Only read part number in Rf.test.cpp before TX and RX

--- Tests/HardwareTests/Rf.test.cpp
+++ Tests/HardwareTests/Rf.test.cpp
@@ -49,32 +49,6 @@ private:
         PRINTF("RF module initialized\n");
 
         PRINTF("\n");
-        auto partInfo = rf::ReadPartInfo();
-        PRINTF("Part info: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x\n",
-               partInfo[0],
-               partInfo[1],
-               partInfo[2],
-               partInfo[3],
-               partInfo[4],
-               partInfo[5],
-               partInfo[6],
-               partInfo[7]);
-
-        PRINTF("\n");
-        auto functionInfo = rf::ReadFunctionInfo();
-        PRINTF("Function info: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x\n",
-               functionInfo[0],
-               functionInfo[1],
-               functionInfo[2],
-               functionInfo[3],
-               functionInfo[4],
-               functionInfo[5]);
-
-        PRINTF("\n");
-        auto deviceState = rf::ReadDeviceState();
-        PRINTF("Device state: 0x%02x 0x%02x\n", deviceState[0], deviceState[1]);
-
-        PRINTF("\n");
         auto partNumber = rf::ReadPartNumber();
         PRINTF("Part number: 0x%4x == 0x%4x\n", partNumber, rf::correctPartNumber);
         Check(partNumber == rf::correctPartNumber);
