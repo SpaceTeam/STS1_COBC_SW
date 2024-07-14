edc1f2e04de4a9091e34866d5e58e08664a27d6b
Patrick Kappl <kappl.pa@gmail.com>
2024-07-14 11:33:53 +0000

    Add rf::ReadModemStatus()

--- Sts1CobcSw/Periphery/Rf.hpp
+++ Sts1CobcSw/Periphery/Rf.hpp
@@ -27,6 +27,7 @@ auto Initialize(TxType txType) -> void;
 auto ReadPartNumber() -> std::uint16_t;
 auto ReadPartInfo() -> std::array<Byte, 8>;
 auto ReadFunctionInfo() -> std::array<Byte, 6>;
+auto ReadModemStatus() -> std::array<Byte, 8>;
 auto ReadDeviceState() -> std::array<Byte, 2>;
 auto SetTxType(TxType txType) -> void;
 auto Send(void const * data, std::size_t nBytes) -> void;
