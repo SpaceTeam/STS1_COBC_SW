f59fe6b2eb709e331d9f5284d742e65e72a68202
Patrick Kappl <kappl.pa@gmail.com>
2024-08-03 19:14:41 +0000

    Fix PersistentVariable
    - Turn PersistentVariable.cpp into PersistentVariable.ipp
    - Change library target type from STATIC to INTERFACE
    - Use fram::Address and fram::Size
    - Correctly use fram::WriteTo() and fram::ReadFrom()
    - Use one CHECK() per test condition since chaining them is not
      supported by Catch2
    - Make cosmetic changes
    - Fix function parameters being passed by value instead of const &
    - Overload operator==() for UserDefined in unit test
    - Initialize FRAM mock in all test cases since ctest runs them as
      individual executables

--- Sts1CobcSw/PersistentState/PersistentVariable.hpp
+++ Sts1CobcSw/PersistentState/PersistentVariable.hpp
@@ -1,23 +1,31 @@
 #pragma once
 
+
+#include <Sts1CobcSw/Periphery/Fram.hpp>
+
 #include <cstdint>
+
 
 namespace sts1cobcsw
 {
-template<typename T, std::uint32_t address, std::uint32_t offset>
+template<typename T, fram::Address address, fram::Size offset>
 class PersistentVariable
 {
 public:
     PersistentVariable();
 
-    auto Store(T t) -> void;
+    auto Store(T const & t) -> void;
     auto Load() -> T;
+
 
 private:
     T t1_ = T{};
     T t2_ = T{};
     T t3_ = T{};
 
-    auto MajorityVote(T vote1, T vote2, T vote3) -> T;
+    auto MajorityVote(T const & vote1, T const & vote2, T const & vote3) -> T;
 };
 }
+
+
+#include <Sts1CobcSw/PersistentState/PersistentVariable.ipp>  // IWYU pragma: keep
