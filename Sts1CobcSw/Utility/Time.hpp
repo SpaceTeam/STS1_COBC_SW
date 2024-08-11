64f87bfb97b880d9746362a8d2396fc5192b7fe7
Tomoya Hagen <tomoya.hagen@spaceteam.at>
2024-08-11 16:20:20 +0200

    Introduce new strong types for time

--- Sts1CobcSw/Utility/Time.hpp
+++ Sts1CobcSw/Utility/Time.hpp
@@ -3,6 +3,8 @@
 
 // TODO: Just including timemodel.h might be better here because then Utility should be able to be
 // combined with Catch2 without defining two main()s.
+#include <strong_type/affine_point.hpp>
+
 #include <rodos_no_using_namespace.h>
 
 #include <cstdint>
@@ -13,6 +15,12 @@ namespace sts1cobcsw::utility
 //! Number of nanoseconds between 01.01.1970 and 01.01.2000
 constexpr auto rodosUnixOffset = 946'684'800 * RODOS::SECONDS;
 
+using Duration = strong::type<std::int64_t, struct DurationTag>;
+using RodosTime = strong::type<std::int64_t, struct RodosTimeTag, strong::affine_point<Duration>>;
+
+using RealTimeDuration = strong::type<std::int32_t, struct RealTimeDurationTag>;
+using RealTime =
+    strong::type<std::int32_t, struct RealTimeTag, strong::affine_point<RealTimeDuration>>;
 
 // Todo: Change name
 //! @brief Print UTC system time in human readable format.
