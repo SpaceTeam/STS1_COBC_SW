00fa557da1ce22170224dad4d48efa146745e652
kamilchmela <chmelakamil@atlas.cz>
2024-09-16 11:59:41 +0000

    Fix errors

--- Tests/HardwareTests/PeripherySupervision.test.cpp
+++ Tests/HardwareTests/PeripherySupervision.test.cpp
@@ -3,34 +3,25 @@
 #include <Sts1CobcSw/Periphery/Rf.hpp>
 #include <Sts1CobcSw/Utility/Span.hpp>
 
+#include <rodos/support/support-libs/random.h>
 #include <rodos_no_using_namespace.h>
 
 
 namespace sts1cobcsw
 {
+
+
 auto delay = 1 * RODOS::SECONDS;
-
-std::uint32_t const flashAddress = 0x00'01'00'00U;
-
-RODOS::setRandSeed(static_cast<std::uint64_t>(RODOS::NOW()));
-constexpr std::uint32_t nAdressBits = 20U;
-auto framAddress = fram::Address{RODOS::uint32Rand() % (1U << nAdressBits)};
-
-constexpr std::size_t testDataSize = 11 * 1024;
-auto framTestData = std::array<Byte, testDataSize>{};
-// Baud rate = 6 MHz, largest data transfer = 11 KiB -> spiTimeout = 30 ms is enough for all
-// transfers
-constexpr auto spiTimeout = 30 * RODOS::MILLISECONDS;
 
 class PeripherySupervisionTest : public RODOS::StaticThread<>
 {
-private:
+public:
     PeripherySupervisionTest() : StaticThread("PeripherySupervisionTest")
     {
     }
 
 
-public:
+private:
     void init() override
     {
     }
@@ -40,18 +31,27 @@ public:
     {
         using RODOS::PRINTF;
 
+        std::uint32_t const flashAddress = 0x00'01'00'00U;
+
+        RODOS::setRandSeed(static_cast<std::uint64_t>(RODOS::NOW()));
+        constexpr std::uint32_t nAdressBits = 20U;
+        auto framAddress = fram::Address{RODOS::uint32Rand() % (1U << nAdressBits)};
+
+        constexpr std::size_t testDataSize = 11 * 1024;
+        auto framTestData = std::array<Byte, testDataSize>{};
+        // Baud rate = 6 MHz, largest data transfer = 11 KiB -> spiTimeout = 30 ms is enough for all
+        // transfers
+        constexpr auto spiTimeout = 30 * RODOS::MILLISECONDS;
+
         RODOS::AT(delay);
 
-        PRINTF("Testing Flash writing\n")
+        PRINTF("Testing Flash writing\n");
         auto page = flash::ReadPage(flashAddress);
         flash::ProgramPage(flashAddress, Span(page));
 
-        PRINTF("Testing Fram writing and reading\n")
+        PRINTF("Testing Fram writing and reading\n");
         fram::WriteTo(framAddress, Span(framTestData), spiTimeout);
-
-        PRINTF("Testing Rf writing\n")
-        rf::SendCommand(Span(page));
     }
 
-} PeripherySupervisionTest;
+} peripherySupervisionTest;
 }
