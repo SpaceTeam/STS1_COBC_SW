a169ba79a97be4a5e3459da88ef003a67c8a0c1f
kamilchmela <chmelakamil@atlas.cz>
2024-09-16 11:59:42 +0000

    Add `FlashMock.cpp/hpp`

---
+++ Sts1CobcSw/Periphery/FlashMock.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include <Sts1CobcSw/Hal/Spi.hpp>
+#include <Sts1CobcSw/Outcome/Outcome.hpp>
+#include <Sts1CobcSw/Periphery/Flash.hpp>
+#include <Sts1CobcSw/Serial/Byte.hpp>
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <span>
+
+
+namespace sts1cobcsw::flash
+{
+auto SetDoInitialize(void (*doInitializeFunction)()) -> void;
+auto SetDoReadJedecId(JedecId (*doReadJedecIdFunction)()) -> void;
+auto SetDoReadStatusRegister(Byte (*doReadStatusRegisterFunction)(std::int8_t registerNo)) -> void;
+
+auto SetDoReadPage(Page (*doReadPageFunction)(std::uint32_t address)) -> void;
+auto SetDoProgramPage(void (*doProgramPageFunction)(std::uint32_t address, PageSpan data)) -> void;
+auto SetDoEraseSector(void (*doEraseSectorFunction)(std::uint32_t address)) -> void;
+auto SetDoWaitWhileBusyFunction(Result<void> (*doWaitWhileBusy)(std::int64_t timeout)) -> void;
+auto SetDoActualBaudRate(std::int32_t (*doActualBaudRateFunction)()) -> void;
+
+namespace empty
+{
+auto SetAllDoFunctions() -> void;
+
+auto DoInitialize() -> void;
+auto DoReadJedecId() -> JedecId;
+auto DoReadStatusRegister(std::int8_t registerNo) -> Byte;
+
+auto DoReadPage(std::uint32_t address) -> Page;
+auto DoProgramPage(std::uint32_t address, PageSpan data) -> void;
+auto DoEraseSector(std::uint32_t address) -> void;
+auto DoWaitWhileBusy(std::int64_t timeout) -> Result<void>;
+auto DoActualBaudRate() -> std::int32_t;
+}
+}
