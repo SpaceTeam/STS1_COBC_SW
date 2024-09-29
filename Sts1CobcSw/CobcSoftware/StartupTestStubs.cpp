51e57a46e6c1697db4eb46e6e048dedc70853245
kamilchmela <chmelakamil@atlas.cz>
2024-09-29 19:19:07 +0000

    Delete TODO comment

--- Sts1CobcSw/CobcSoftware/StartupTestStubs.cpp
+++ Sts1CobcSw/CobcSoftware/StartupTestStubs.cpp
@@ -3,8 +3,6 @@
 
 namespace sts1cobcsw
 {
-// TODO: These functions must also set fram::framIsWorking, as well as "flashIsWorking" and
-// "rfIsWorking" to true.
 auto ResumeFlashStartupTestThread() -> void
 {
     persistentVariables.template Store<"flashIsWorking">(true);
