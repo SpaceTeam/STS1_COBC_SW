678be9348429b83fa96191addba83bce0fb71e13
Patrick Kappl <kappl.pa@gmail.com>
2024-10-05 17:20:40 +0000

    Get rid of `USE_NO_RODOS` and rework `LfsWrapper.test.cpp`
    Eventually, the `File` class will need semaphores and therefore require
    the main function from Rodos. I converted it now, because that allows me
    to get rid of this `USE_NO_RODOS` nonsense in `Outcome.hpp` too.

--- Sts1CobcSw/Outcome/Outcome.hpp
+++ Sts1CobcSw/Outcome/Outcome.hpp
@@ -1,64 +1,16 @@
 #pragma once
 
-#ifndef USE_NO_RODOS
-    #include <rodos_no_using_namespace.h>
-#else
-    #include <iostream>
-#endif
 
+#include <rodos_no_using_namespace.h>
 
 #if defined(SYSTEM_ERROR2_NOT_POSIX)
     // NOLINTNEXTLINE(*macro-usage)
-    #ifdef USE_NO_RODOS
-        #define SYSTEM_ERROR2_FATAL(msg)
-    #else
-        #define SYSTEM_ERROR2_FATAL(msg) RODOS::hwResetAndReboot()
-    #endif
+    #define SYSTEM_ERROR2_FATAL(msg) RODOS::hwResetAndReboot()
 #endif
 
 #include <outcome-experimental.hpp>  // IWYU pragma: export
 
-#ifdef USE_NO_RODOS
-// First define a policy
-struct RebootPolicy : outcome_v2::experimental::policy::base
-{
-    template<class Impl>
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    static constexpr void wide_value_check(Impl && self)
-    {
-        if(!base::_has_value(std::forward<Impl>(self)))
-        {
-            std::cout << "Error in wide_value_check(): _has_value() returned false. "
-                      << "Calling std::abort().\n ";
-            std::abort();
-        }
-    }
 
-    template<class Impl>
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    static constexpr void wide_error_check(Impl && self)
-    {
-        if(!base::_has_error(std::forward<Impl>(self)))
-        {
-            std::cout << "Error in wide_error_check(): _has_error() returned false. "
-                      << "Calling std::abort().\n ";
-            std::abort();
-        }
-    }
-
-    template<class Impl>
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    static constexpr void wide_exception_check(Impl && self)
-    {
-        if(!base::_has_exception(std::forward<Impl>(self)))
-        {
-            std::cout << "Error in wide_exception_check(): _has_exception() returned false. "
-                      << "Calling std::abort().\n ";
-            std::abort();
-        }
-    }
-};
-#else
 struct RebootPolicy : outcome_v2::experimental::policy::base
 {
     template<class Impl>
@@ -69,11 +21,9 @@ struct RebootPolicy : outcome_v2::experimental::policy::base
         //! contain a value
         if(!base::_has_value(std::forward<Impl>(self)))
         {
-    #ifndef USE_NO_RODOS
             RODOS::PRINTF(
                 "Error: The value is not present. Performing hardware reset and reboot.\n");
             RODOS::hwResetAndReboot();
-    #endif
         }
     }
 
@@ -85,11 +35,9 @@ struct RebootPolicy : outcome_v2::experimental::policy::base
         //! contain an error
         if(!base::_has_error(std::forward<Impl>(self)))
         {
-    #ifndef USE_NO_RODOS
             RODOS::PRINTF(
                 "Error: The error is not present. Performing hardware reset and reboot.\n");
             RODOS::hwResetAndReboot();
-    #endif
         }
     }
 
@@ -99,12 +47,9 @@ struct RebootPolicy : outcome_v2::experimental::policy::base
     {
         if(!base::_has_exception(std::forward<Impl>(self)))
         {
-    #ifndef USE_NO_RODOS
             RODOS::PRINTF(
                 "Error: The exception is not present. Performing hardware reset and reboot.\n");
             RODOS::hwResetAndReboot();
-    #endif
         }
     }
 };
-#endif
--- Tests/UnitTests/LfsWrapper.test.cpp
+++ Tests/UnitTests/LfsWrapper.test.cpp
@@ -1,68 +1,79 @@
+#include <Tests/UnitTests/UnitTestThread.hpp>
+
 #include <Sts1CobcSw/FileSystem/ErrorsAndResult.hpp>
 #include <Sts1CobcSw/FileSystem/LfsMemoryDevice.hpp>
 #include <Sts1CobcSw/FileSystem/LfsWrapper.hpp>
 #include <Sts1CobcSw/Serial/Byte.hpp>
 #include <Sts1CobcSw/Utility/Span.hpp>
 
-#include <catch2/catch_test_macros.hpp>
-
 #include <littlefs/lfs.h>
+
+#include <etl/string.h>
 
 #include <algorithm>
 #include <array>
 
 using sts1cobcsw::Byte;
 using sts1cobcsw::Span;
+using sts1cobcsw::fs::Path;
 using sts1cobcsw::operator""_b;  // NOLINT(misc-unused-using-decls)
 
 
-TEST_CASE("LfsWrapper")
+auto RunUnitTest() -> void
 {
+    Require(true);
+
     sts1cobcsw::fs::Initialize();
     auto mountResult = sts1cobcsw::fs::Mount();
-    CHECK(not mountResult.has_error());
+    Require(not mountResult.has_error());
 
-    auto const * filePath = "/MyFile";
+    auto filePath = Path("/MyFile");
     auto openResult = sts1cobcsw::fs::Open(filePath, LFS_O_WRONLY | LFS_O_CREAT);
-    CHECK(openResult.has_value());
-
+    Require(openResult.has_value());
     auto & writeableFile = openResult.value();
 
+    // Reopening the file should fail
     auto reopenResult = sts1cobcsw::fs::Open(filePath, LFS_O_WRONLY | LFS_O_CREAT);
-    CHECK(reopenResult.has_error());  // open should fail because the file is already open
+    Require(reopenResult.has_error());
+
+    // Empty file should have size 0
+    auto sizeResult = writeableFile.Size();
+    Require(sizeResult.has_value());
+    Require(sizeResult.value() == 0);
 
     auto writeData = std::array{0xAA_b, 0xBB_b, 0xCC_b, 0xDD_b};
     auto writeResult = writeableFile.Write(Span(writeData));
-    CHECK(writeResult.has_value());
-    CHECK(writeResult.value() == sizeof(writeData));
+    Require(writeResult.has_value());
+    Require(writeResult.value() == sizeof(writeData));
 
+    // Read() should fail since the file is only opened for writing
     auto readData = std::array{0x11_b, 0x22_b, 0x33_b, 0x44_b};
     auto readResult = writeableFile.Read(Span(&readData));
-    CHECK(readResult.has_error());  // read file should fail as LFS_O_WRONLY flag used
+    Require(readResult.has_error());
 
     auto closeResult = writeableFile.Close();
-    CHECK(not closeResult.has_error());
+    Require(not closeResult.has_error());
 
-    openResult = sts1cobcsw::fs::Open(filePath, LFS_O_RDONLY | LFS_O_CREAT);
-    CHECK(openResult.has_value());
-
+    openResult = sts1cobcsw::fs::Open(filePath, LFS_O_RDONLY);
+    Require(openResult.has_value());
     auto & readableFile = openResult.value();
 
-    auto sizeResult = readableFile.Size();
-    CHECK(sizeResult.has_value());
-    CHECK(sizeResult.value() == sizeof(int));
+    sizeResult = readableFile.Size();
+    Require(sizeResult.has_value());
+    Require(sizeResult.value() == sizeof(writeData));
 
     readResult = readableFile.Read(Span(&readData));
-    CHECK(readResult.has_value());
-    CHECK(readResult.value() == sizeof(writeData));
-    CHECK(readData == writeData);
+    Require(readResult.has_value());
+    Require(readResult.value() == sizeof(readData));
+    Require(readData == writeData);
 
+    // Write() should fail since the file is only opened for reading
     writeResult = readableFile.Write(Span(writeData));
-    CHECK(writeResult.has_error());  // write file should fail as LFS_O_RDONLY flag used
+    Require(writeResult.has_error());
 
     closeResult = readableFile.Close();
-    CHECK(not closeResult.has_error());
+    Require(not closeResult.has_error());
 
     auto unmountResult = sts1cobcsw::fs::Unmount();
-    CHECK(not unmountResult.has_error());
+    Require(not unmountResult.has_error());
 }
