99906fdae0813dd37245a1f08b3521cb2cecae02
Patrick Kappl <kappl.pa@gmail.com>
2024-10-08 18:49:01 +0000

    Fix bug in `operator==(S, S)` and refactor FRAM ring array test

--- Tests/UnitTests/FramRingArray.test.cpp
+++ Tests/UnitTests/FramRingArray.test.cpp
@@ -1,15 +1,12 @@
 #include <Tests/UnitTests/UnitTestThread.hpp>
 
-#include <Sts1CobcSw/Edu/ProgramStatusHistory.hpp>
 #include <Sts1CobcSw/FramSections/RingArray.hpp>
 #include <Sts1CobcSw/FramSections/Section.hpp>
 #include <Sts1CobcSw/Periphery/Fram.hpp>
 #include <Sts1CobcSw/Periphery/FramMock.hpp>
-#include <Sts1CobcSw/ProgramId/ProgramId.hpp>
 #include <Sts1CobcSw/Serial/Byte.hpp>
 #include <Sts1CobcSw/Serial/Serial.hpp>
 #include <Sts1CobcSw/Utility/ErrorDetectionAndCorrection.hpp>
-#include <Sts1CobcSw/Utility/TimeTypes.hpp>
 
 #include <strong_type/equality.hpp>
 #include <strong_type/type.hpp>
@@ -24,19 +21,21 @@
 
 namespace fram = sts1cobcsw::fram;
 using sts1cobcsw::operator""_b;  // NOLINT(misc-unused-using-decls)
+using sts1cobcsw::totalSerialSize;
+
 
 struct S
 {
     std::uint16_t u16 = 0;
     std::int32_t i32 = 0;
     std::uint8_t u8 = 0;
+
+    friend auto operator==(S const & lhs, S const & rhs) -> bool
+    {
+        return (lhs.i32 == rhs.i32) and (lhs.u16 == rhs.u16) and (lhs.u8 == rhs.u8);
+    }
 };
 
-// NOLINTNEXTLINE(modernize-use-trailing-return-type)
-bool operator==(S const & lhs, S const & rhs)
-{
-    return ((lhs.i32 == rhs.i32) and (lhs.u16 == rhs.u16) and (lhs.u8 and rhs.u8));
-}
 
 namespace sts1cobcsw
 {
@@ -45,51 +44,36 @@ constexpr std::size_t serialSize<S> =
     totalSerialSize<decltype(S::u16), decltype(S::i32), decltype(S::u8)>;
 }
 
-template<std::endian endianness>
-auto SerializeTo(void * destination, S const & data) -> void *
-{
-    destination = sts1cobcsw::SerializeTo<endianness>(destination, data.u16);
-    destination = sts1cobcsw::SerializeTo<endianness>(destination, data.i32);
-    destination = sts1cobcsw::SerializeTo<endianness>(destination, data.u8);
-    return destination;
-}
 
 template<std::endian endianness>
-auto DeserializeFrom(void const * source, S * data) -> void const *
-{
-    source = sts1cobcsw::DeserializeFrom<endianness>(source, &(data->u16));
-    source = sts1cobcsw::DeserializeFrom<endianness>(source, &(data->i32));
-    source = sts1cobcsw::DeserializeFrom<endianness>(source, &(data->u8));
-    return source;
-}
+auto SerializeTo(void * destination, S const & data) -> void *;
+template<std::endian endianness>
+auto DeserializeFrom(void const * source, S * data) -> void const *;
 
 
-inline constexpr auto section =
+inline constexpr auto charSection =
     sts1cobcsw::Section<fram::Address(0), fram::Size(3 * 2 * sizeof(std::size_t) + 4)>{};
+inline constexpr auto sSection =
+    sts1cobcsw::Section<charSection.end,
+                        fram::Size(3 * 2 * sizeof(std::size_t) + 4 * totalSerialSize<S>)>{};
 
-inline constexpr auto customTypeSection =
-    sts1cobcsw::Section<fram::Address(3 * 2 * sizeof(std::size_t) + 4),
-                        fram::Size(3 * 2 * sizeof(std::size_t) + sts1cobcsw::serialSize<S> * 4)>{};
-
-inline constexpr auto charRingArray = sts1cobcsw::RingArray<char, section, 2>{};
-inline constexpr auto sRingArray = sts1cobcsw::RingArray<S, customTypeSection, 2>{};
+inline constexpr auto charRingArray = sts1cobcsw::RingArray<char, charSection, 2>{};
+inline constexpr auto sRingArray = sts1cobcsw::RingArray<S, sSection, 2>{};
 
 
 static_assert(std::is_same_v<decltype(charRingArray)::ValueType, char>);
 static_assert(charRingArray.FramCapacity() == 3);
 static_assert(charRingArray.CacheCapacity() == 2);
 static_assert(charRingArray.section.begin == fram::Address(0));
-static_assert(charRingArray.section.end == fram::Address(28));
 static_assert(charRingArray.section.size == fram::Size(28));
+static_assert(charRingArray.section.end == fram::Address(28));
 
 static_assert(std::is_same_v<decltype(sRingArray)::ValueType, S>);
 static_assert(sRingArray.FramCapacity() == 3);
 static_assert(sRingArray.CacheCapacity() == 2);
 static_assert(sRingArray.section.begin == fram::Address(28));
-static_assert(sRingArray.section.end
-              == fram::Address(28 + 3 * 2 * sizeof(size_t) + 4 * sts1cobcsw::serialSize<S>));
-static_assert(sRingArray.section.size
-              == fram::Size(3 * 2 * sizeof(size_t) + 4 * sts1cobcsw::serialSize<S>));
+static_assert(sRingArray.section.size == fram::Size(52));
+static_assert(sRingArray.section.end == fram::Address(80));
 
 
 auto RunUnitTest() -> void
@@ -98,10 +82,9 @@ auto RunUnitTest() -> void
 
     fram::ram::SetAllDoFunctions();
     fram::Initialize();
-    static constexpr auto ringArrayStartAddress = 3 * 2 * sizeof(std::size_t);
+    static constexpr auto charRingArrayStartAddress = 3 * 2 * sizeof(std::size_t);
     static constexpr auto sRingArrayStartAddress =
-        3 * 2 * sizeof(std::size_t) + 4 + 3 * 2 * sizeof(std::size_t);
-
+        value_of(charRingArray.section.end) + 3 * 2 * sizeof(std::size_t);
 
     // SECTION("FRAM is working")
     {
@@ -133,10 +116,10 @@ auto RunUnitTest() -> void
         Require(charRingArray.Get(1) == 12);
         Require(charRingArray.Get(2) == 13);
 
-        // PushBack writes to memory
-        Require(fram::ram::memory[ringArrayStartAddress + 0] == 11_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 1] == 12_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 2] == 13_b);
+        // PushBack() writes to memory
+        Require(fram::ram::memory[charRingArrayStartAddress + 0] == 11_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 1] == 12_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 2] == 13_b);
 
         // When pushing to a full ring, the size stays the same and the oldest element is lost
         charRingArray.PushBack(14);
@@ -150,10 +133,10 @@ auto RunUnitTest() -> void
         Require(charRingArray.Size() == 3);
         Require(charRingArray.Front() == 13);
         Require(charRingArray.Back() == 15);
-        Require(fram::ram::memory[ringArrayStartAddress + 0] == 15_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 1] == 12_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 2] == 13_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 3] == 14_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 0] == 15_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 1] == 12_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 2] == 13_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 3] == 14_b);
 
         // Set() writes to memory
         charRingArray.Set(0, 21);
@@ -162,10 +145,10 @@ auto RunUnitTest() -> void
         Require(charRingArray.Get(0) == 21);
         Require(charRingArray.Get(1) == 22);
         Require(charRingArray.Get(2) == 23);
-        Require(fram::ram::memory[ringArrayStartAddress + 0] == 23_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 1] == 12_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 2] == 21_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 3] == 22_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 0] == 23_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 1] == 12_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 2] == 21_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 3] == 22_b);
 
         // Get() with out-of-bounds index prints a debug message and returns the last element
         Require(charRingArray.Get(17) == 23);
@@ -198,9 +181,9 @@ auto RunUnitTest() -> void
         Require(charRingArray.Get(0) == 11);
         Require(charRingArray.Get(1) == 12);
 
-        // PushBack does not write to memory
-        Require(fram::ram::memory[ringArrayStartAddress + 0] == 0_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 1] == 0_b);
+        // PushBack() does not write to memory
+        Require(fram::ram::memory[charRingArrayStartAddress + 0] == 0_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 1] == 0_b);
 
         // When pushing to a full ring, the size stays the same and the oldest element is lost
         charRingArray.PushBack(13);
@@ -213,8 +196,8 @@ auto RunUnitTest() -> void
         charRingArray.Set(1, 22);
         Require(charRingArray.Get(0) == 21);
         Require(charRingArray.Get(1) == 22);
-        Require(fram::ram::memory[ringArrayStartAddress + 0] == 0_b);
-        Require(fram::ram::memory[ringArrayStartAddress + 1] == 0_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 0] == 0_b);
+        Require(fram::ram::memory[charRingArrayStartAddress + 1] == 0_b);
 
         // Get() with out-of-bounds index prints a debug message and returns the last element
         Require(charRingArray.Get(17) == 22);
@@ -224,21 +207,10 @@ auto RunUnitTest() -> void
         Require(charRingArray.Get(1) == 22);
     }
 
-
-    // SECTION("ProgramStatusHistoryEntry test")
+    // SECTION("RingArray of custom type")
     {
-        using sts1cobcsw::serialSize;
-        using sts1cobcsw::edu::ProgramStatusHistoryEntry;
-
-        using sts1cobcsw::ProgramId;
-        using sts1cobcsw::RealTime;
-        using sts1cobcsw::edu::ProgramStatus;
-
-
         memory.fill(0x00_b);
         fram::framIsWorking.Store(true);
-
-        Require(sRingArray.Size() == 0);
 
         auto s1 = S{.u16 = 1, .i32 = 1, .u8 = 1};
         auto s2 = S{.u16 = 2, .i32 = 2, .u8 = 2};
@@ -261,7 +233,6 @@ auto RunUnitTest() -> void
         Require(sRingArray.Front() == s1);
         Require(sRingArray.Back() == s1);
 
-        // programStatusHistory.PushBack(entry2);
         sRingArray.PushBack(s2);
         Require(sRingArray.Size() == 2);
         Require(sRingArray.Front() == s1);
@@ -276,7 +247,7 @@ auto RunUnitTest() -> void
         Require(sRingArray.Get(1) == s2);
         Require(sRingArray.Get(2) == s3);
 
-        // PushBack writes to memory
+        // PushBack() writes to memory
         Require(fram::ram::memory[sRingArrayStartAddress + 0] == 1_b);
         Require(fram::ram::memory[sRingArrayStartAddress + sizeof(S::u16)] == 1_b);
         Require(fram::ram::memory[sRingArrayStartAddress + sizeof(S::u16) + sizeof(S::i32)] == 1_b);
@@ -303,9 +274,9 @@ auto RunUnitTest() -> void
         Require(sRingArray.Get(2) == s8);
 
         Require(fram::ram::memory[sRingArrayStartAddress + 0] == 8_b);
-        Require(fram::ram::memory[sRingArrayStartAddress + serialSize<S>] == 2_b);
-        Require(fram::ram::memory[sRingArrayStartAddress + 2 * serialSize<S>] == 6_b);
-        Require(fram::ram::memory[sRingArrayStartAddress + 3 * serialSize<S>] == 7_b);
+        Require(fram::ram::memory[sRingArrayStartAddress + totalSerialSize<S>] == 2_b);
+        Require(fram::ram::memory[sRingArrayStartAddress + 2 * totalSerialSize<S>] == 6_b);
+        Require(fram::ram::memory[sRingArrayStartAddress + 3 * totalSerialSize<S>] == 7_b);
 
         // Get() with out-of-bounds index prints a debug message and returns the last element
         Require(sRingArray.Get(17) == s8);
@@ -315,4 +286,24 @@ auto RunUnitTest() -> void
         Require(sRingArray.Get(1) == s7);
         Require(sRingArray.Get(2) == s8);
     }
+}
+
+
+template<std::endian endianness>
+auto SerializeTo(void * destination, S const & data) -> void *
+{
+    destination = sts1cobcsw::SerializeTo<endianness>(destination, data.u16);
+    destination = sts1cobcsw::SerializeTo<endianness>(destination, data.i32);
+    destination = sts1cobcsw::SerializeTo<endianness>(destination, data.u8);
+    return destination;
+}
+
+
+template<std::endian endianness>
+auto DeserializeFrom(void const * source, S * data) -> void const *
+{
+    source = sts1cobcsw::DeserializeFrom<endianness>(source, &(data->u16));
+    source = sts1cobcsw::DeserializeFrom<endianness>(source, &(data->i32));
+    source = sts1cobcsw::DeserializeFrom<endianness>(source, &(data->u8));
+    return source;
 }
