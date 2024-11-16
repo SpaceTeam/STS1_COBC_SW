7dd468b957cb6bbac8556bdb0c6862f490a99d34
Patrick Kappl <kappl.pa@gmail.com>
2024-11-16 15:26:30 +0000

    Add stubs for startup test threads
    The resume functions were not enough. We really needed the threads.

--- Sts1CobcSw/CobcSoftware/StartupTestStubs.cpp
+++ Sts1CobcSw/CobcSoftware/StartupTestStubs.cpp
@@ -1,28 +1,103 @@
 #include <Sts1CobcSw/CobcSoftware/SpiStartupTestAndSupervisorThread.hpp>
+#include <Sts1CobcSw/CobcSoftware/ThreadPriorities.hpp>
 #include <Sts1CobcSw/FramSections/FramLayout.hpp>
 #include <Sts1CobcSw/Periphery/Fram.hpp>
+#include <Sts1CobcSw/Utility/Debug.hpp>
+
+#include <rodos_no_using_namespace.h>
 
 
 namespace sts1cobcsw
 {
-auto ResumeFlashStartupTestThread() -> void
+auto const stackSize = 800U;
+
+
+class FramEpsStartupTestThread : public RODOS::StaticThread<stackSize>
 {
-    persistentVariables.template Store<"flashIsWorking">(true);
-    ResumeSpiStartupTestAndSupervisorThread();
-}
+public:
+    FramEpsStartupTestThread()
+        : StaticThread("FramEpsStartupTestThread", framEpsStartupTestThreadPriority)
+    {
+    }
+
+private:
+    void init() override
+    {
+    }
+
+    void run() override
+    {
+        SuspendUntil(endOfTime);
+        DEBUG_PRINT("FRAM/EPS start-up test ...");
+        fram::framIsWorking.Store(true);
+        persistentVariables.template Store<"epsIsWorking">(true);
+        ResumeSpiStartupTestAndSupervisorThread();
+        SuspendUntil(endOfTime);
+    }
+} framEpsStartupTestThread;
+
+
+class FlashStartupTestThread : public RODOS::StaticThread<stackSize>
+{
+public:
+    FlashStartupTestThread()
+        : StaticThread("FlashStartupTestThread", flashStartupTestThreadPriority)
+    {
+    }
+
+private:
+    void init() override
+    {
+    }
+
+    void run() override
+    {
+        SuspendUntil(endOfTime);
+        DEBUG_PRINT("Flash start-up test ...");
+        persistentVariables.template Store<"flashIsWorking">(true);
+        ResumeSpiStartupTestAndSupervisorThread();
+        SuspendUntil(endOfTime);
+    }
+} flashStartupTestThread;
+
+
+class RfStartupTestThread : public RODOS::StaticThread<stackSize>
+{
+public:
+    RfStartupTestThread() : StaticThread("RfStartupTestThread", rfStartupTestThreadPriority)
+    {
+    }
+
+private:
+    void init() override
+    {
+    }
+
+    void run() override
+    {
+        SuspendUntil(endOfTime);
+        DEBUG_PRINT("RF start-up test ...");
+        persistentVariables.template Store<"rfIsWorking">(true);
+        ResumeSpiStartupTestAndSupervisorThread();
+        SuspendUntil(endOfTime);
+    }
+} rfStartupTestThread;
 
 
 auto ResumeFramEpsStartupTestThread() -> void
 {
-    fram::framIsWorking.Store(true);
-    persistentVariables.template Store<"epsIsWorking">(true);
-    ResumeSpiStartupTestAndSupervisorThread();
+    framEpsStartupTestThread.resume();
+}
+
+
+auto ResumeFlashStartupTestThread() -> void
+{
+    flashStartupTestThread.resume();
 }
 
 
 auto ResumeRfStartupTestThread() -> void
 {
-    persistentVariables.template Store<"rfIsWorking">(true);
-    ResumeSpiStartupTestAndSupervisorThread();
+    rfStartupTestThread.resume();
 }
 }
