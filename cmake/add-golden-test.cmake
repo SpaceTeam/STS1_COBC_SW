44af090ece2a0258e39327c91c4af2150cafa313
Patrick Kappl <kappl.pa@gmail.com>
2024-12-01 14:25:19 +0000

    Allow build type specific expected output files for golden tests
    Add one for the SPI supervisor test.

--- cmake/add-golden-test.cmake
+++ cmake/add-golden-test.cmake
@@ -11,12 +11,22 @@ function(add_golden_test test_file)
     target_include_directories(${target} ${warning_guard} PUBLIC "${CMAKE_SOURCE_DIR}")
     set_target_properties(${target} PROPERTIES OUTPUT_NAME ${test_file_name})
 
+    # Check if an expected output file exists for the current build type
+    set(expected_output_directory "${CMAKE_SOURCE_DIR}/Tests/GoldenTests/ExpectedOutputs")
+    set(expected_output_file
+        "${expected_output_directory}/${test_file_name}_${CMAKE_BUILD_TYPE}.txt"
+    )
+    if(NOT EXISTS ${expected_output_file})
+        # If not, use the default expected output file
+        set(expected_output_file "${expected_output_directory}/${test_file_name}.txt")
+    endif()
+
     add_test(
         NAME ${test_file_name}
         COMMAND
             "${CMAKE_COMMAND}" -D "TEST_EXECUTABLE=$<TARGET_FILE:${target}>" -D
-            "EXPECTED_OUTPUT_FILE=${CMAKE_SOURCE_DIR}/Tests/GoldenTests/ExpectedOutputs/${test_file_name}.txt"
-            -P "${CMAKE_SOURCE_DIR}/cmake/run-golden-test.cmake"
+            "EXPECTED_OUTPUT_FILE=${expected_output_file}" -P
+            "${CMAKE_SOURCE_DIR}/cmake/run-golden-test.cmake"
     )
     # Set a timeout in case we have deadlocks, infinite loops, etc.
     set_tests_properties(${test_file_name} PROPERTIES TIMEOUT 8)
