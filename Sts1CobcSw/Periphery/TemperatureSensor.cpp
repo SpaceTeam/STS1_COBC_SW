5f8bfd86f92ae2d7c9f6104e3497fc570d43bdd8
Marcus Roth <168829014+M-C-machts@users.noreply.github.com>
2025-01-09 21:18:27 +0000

    Create quick/dirty testcode for stm32 temperature

--- Sts1CobcSw/Periphery/TemperatureSensor.cpp
+++ Sts1CobcSw/Periphery/TemperatureSensor.cpp
@@ -6,9 +6,21 @@
 
 #include <rodos_no_using_namespace.h>
 
+/*
+3.30 Temperature sensor
+The temperature sensor is internally
+connected to the ADC_IN18 input channel which is used to convert the sensor output
+voltage into a digital value. Refer to the reference manual for additional information.
+
+no ADC_IN18 only ADC_CH_018
+
+*/
 
 namespace sts1cobcsw::rftemperaturesensor
 {
+
+
+
 // RF_TMP is read on pin PC0 on internal ADC1
 auto adc = RODOS::HAL_ADC(RODOS::ADC_IDX1);
 constexpr auto channel = RODOS::ADC_CH_010;
@@ -33,4 +45,32 @@ auto Read() -> std::uint16_t
     }
     return rfTemperature;
 }
+
+
+
+// copy code from RF_TMP Sensor as quick and dirty test
+auto adcStm32Temperature = RODOS::HAL_ADC(RODOS::ADC_IDX2);
+constexpr auto channelStm32Temperature = RODOS::ADC_CH_018;
+
+auto InitializeStm32Temperature() -> void
+{
+    adcStm32Temperature.init(channelStm32Temperature);
+    const int32_t bitResolution = 12;  // 3.3 V / 2^12 bits / (10 mV/°C) = 0.0806 °C/bit
+    adcStm32Temperature.config(RODOS::ADC_PARAMETER_RESOLUTION, bitResolution);
+}
+
+auto ReadStm32Temperature() -> std::uint16_t
+{
+    auto stm32Temperature = adcStm32Temperature.read(channelStm32Temperature);
+    // if(rfTemperature == static_cast<std::uint16_t>(RODOS::ADC_ERR_CONV_FAIL))
+    // {
+    //     adc.reset();
+    //     Initialize();
+    //     rfTemperature = adc.read(channel);
+    // }
+    return stm32Temperature;
+}
+
+
+
 }
--- Sts1CobcSw/Periphery/TemperatureSensor.hpp
+++ Sts1CobcSw/Periphery/TemperatureSensor.hpp
@@ -8,4 +8,7 @@ namespace sts1cobcsw::rftemperaturesensor
 {
 auto Initialize() -> void;
 auto Read() -> std::uint16_t;
+
+auto InitializeStm32Temperature() -> void;
+auto ReadStm32Temperature() -> std::uint16_t;
 }
