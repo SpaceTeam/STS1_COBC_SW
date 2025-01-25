ba744eec51e60228a9a24f3c6d3ec7e1fe0cd342
Patrick Kappl <kappl.pa@gmail.com>
2025-01-25 20:49:15 +0000

    STASH: Add radio_config_Si4463.h and si446x_patch.h

--- Tests/HardwareTests/Rf.test.cpp
+++ Tests/HardwareTests/Rf.test.cpp
@@ -53,19 +53,19 @@ private:
         PRINTF("Part number: 0x%4x == 0x%4x\n", partNumber, rf::correctPartNumber);
         Check(partNumber == rf::correctPartNumber);
 
-        PRINTF("\n");
-        auto n = 1;
-        auto message = std::string_view(
-            "123456789,123456789,123456789,123456789,123456789,123456789,123456789,123456789,"
-            "123456789,123456789");
-        PRINTF("Sending a %i bytes long test message %i time(s)\n", message.size(), n);
-        for(int i = 0; i < n; ++i)
-        {
-            led1GpioPin.Set();
-            rf::Send(message.data(), message.size());
-            led1GpioPin.Reset();
-        }
-        PRINTF("  done\n");
+        // PRINTF("\n");
+        // auto n = 1;
+        // auto message = std::string_view(
+        //     "123456789,123456789,123456789,123456789,123456789,123456789,123456789,123456789,"
+        //     "123456789,123456789");
+        // PRINTF("Sending a %i bytes long test message %i time(s)\n", message.size(), n);
+        // for(int i = 0; i < n; ++i)
+        // {
+        //     led1GpioPin.Set();
+        //     rf::Send(message.data(), message.size());
+        //     led1GpioPin.Reset();
+        // }
+        // PRINTF("  done\n");
 
         PRINTF("\n");
         PRINTF("Waiting to receive test data\n");
