8c7cb71a225dd95dd629c65a743da9595489aa03
Patrick Kappl <kappl.pa@gmail.com>
2025-02-08 20:18:55 +0000

    Ignore negative counts when generating coverage info
    For some reason generating the coverage report yields a negative count
    somewhere in `PersistentVariables.ipp`. I don't know why this happens.
    Compiling with `-fprofile-update=atomic` as suggested by the error
    message doesn't help. It only makes the SPI supervisor test require ~100
    bytes more stack without getting rid of the error. Therefore, I followed
    the second suggestion and changed the coverage command to ignore such
    negative counts.

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,8 +1,9 @@
 # ---- Variables ----
 
 # We use variables separate from what CTest uses, because those have customization issues
-set(COVERAGE_TRACE_COMMAND lcov -c -q -o "${PROJECT_BINARY_DIR}/coverage.info" -d
-                           "${PROJECT_BINARY_DIR}" --include "${PROJECT_SOURCE_DIR}/*"
+set(COVERAGE_TRACE_COMMAND
+    lcov -c -q -o "${PROJECT_BINARY_DIR}/coverage.info" -d "${PROJECT_BINARY_DIR}" --include
+    "${PROJECT_SOURCE_DIR}/*" --ignore-errors negative
     CACHE STRING "; separated command to generate a trace for the 'coverage' target"
 )
 
