c998e988b1fdba300e36b1f27449b10e5a6b1f5c
Patrick Kappl <kappl.pa@gmail.com>
2025-02-09 21:48:42 +0000

    Rework FRAM test to use CatchRodos

--- Tests/HardwareTests/Fram.test.cpp
+++ Tests/HardwareTests/Fram.test.cpp
@@ -1,9 +1,9 @@
-#include <Tests/HardwareTests/Utility.hpp>
+#include <Tests/CatchRodos/TestMacros.hpp>
 
 #include <Sts1CobcSw/Periphery/Fram.hpp>
 #include <Sts1CobcSw/Serial/Byte.hpp>
+#include <Sts1CobcSw/Utility/RodosTime.hpp>
 #include <Sts1CobcSw/Utility/Span.hpp>
-#include <Sts1CobcSw/Utility/TimeTypes.hpp>
 
 #include <strong_type/difference.hpp>
 #include <strong_type/type.hpp>
@@ -13,131 +13,43 @@
 
 #include <algorithm>
 #include <array>
-#include <cinttypes>
-#include <cstddef>
 #include <cstdint>
-#include <span>
 
 
-namespace sts1cobcsw
-{
-using RODOS::PRINTF;
+namespace fram = sts1cobcsw::fram;
+
+using sts1cobcsw::Byte;
+using sts1cobcsw::ms;
+using sts1cobcsw::Span;
 using sts1cobcsw::operator""_b;  // NOLINT(misc-unused-using-decls)
 
 
-constexpr std::size_t testDataSize = 11 * 1024;  // 11 KiB
-auto testData = std::array<Byte, testDataSize>{};
-auto readData = std::array<Byte, testDataSize>{};
-// Baud rate = 6 MHz, largest data transfer = 11 KiB -> spiTimeout = 30 ms is enough for all
-// transfers
-// TODO: Test if the test still works if we include Time.hpp and use ms here instead
-constexpr auto spiTimeout = 30 * Duration(RODOS::MILLISECONDS);
-
-
-auto PrintDeviceId(fram::DeviceId const & deviceId) -> void;
-auto WriteAndReadTestData(fram::Address address) -> void;
-
-
-class FramTest : public RODOS::StaticThread<>
+TEST_CASE("FRAM")
 {
-public:
-    FramTest() : StaticThread("FramTest")
-    {
-    }
+    fram::Initialize();
+    auto actualBaudRate = fram::ActualBaudRate();
+    CHECK(actualBaudRate == 6'000'000);
 
+    auto deviceId = fram::ReadDeviceId();
+    CHECK(deviceId == fram::correctDeviceId);
 
-private:
-    void init() override
-    {
-    }
+    RODOS::setRandSeed(static_cast<std::uint64_t>(RODOS::NOW()));
+    auto address = fram::Address(RODOS::uint32Rand() % (value_of(fram::memorySize)));
+    static constexpr auto dataSize = 16 * 1024U;
+    auto writtenData = std::array<Byte, dataSize>{};
+    auto readData = std::array<Byte, dataSize>{};
+    // The timeout doesn't matter because we don't run the SPI supervisor thread in this test
+    fram::WriteTo(address, Span(writtenData), 30 * ms);
+    fram::ReadFrom(address, Span(&readData), 30 * ms);
+    CHECK(readData == writtenData);
 
+    writtenData.fill(0xFF_b);
+    fram::WriteTo(address, Span(writtenData), 30 * ms);
+    fram::ReadFrom(address, Span(&readData), 30 * ms);
+    CHECK(readData == writtenData);
 
-    void run() override
-    {
-        PRINTF("\nFRAM test\n\n");
-
-        PRINTF("\n");
-        fram::Initialize();
-        auto actualBaudRate = fram::ActualBaudRate();
-        PRINTF("Actual baud rate: %" PRIi32 "\n", actualBaudRate);
-
-        PRINTF("\n");
-        auto deviceId = fram::ReadDeviceId();
-        PRINTF("Device ID: ");
-        PrintDeviceId(deviceId);
-        PRINTF(" ==\n");
-        PRINTF("           ");
-        PrintDeviceId(fram::correctDeviceId);
-        Check(deviceId == fram::correctDeviceId);
-
-        RODOS::setRandSeed(static_cast<std::uint64_t>(RODOS::NOW()));
-        constexpr std::uint32_t nAdressBits = 20U;
-        auto address = fram::Address(RODOS::uint32Rand() % (1U << nAdressBits));
-
-        PRINTF("\n");
-        WriteAndReadTestData(address);
-        std::fill(testData.begin(), testData.end(), 0xFF_b);
-        WriteAndReadTestData(address);
-        std::fill(testData.begin(), testData.end(), 0x00_b);
-        WriteAndReadTestData(address);
-    }
-} framTest;
-
-
-auto PrintDeviceId(fram::DeviceId const & deviceId) -> void
-{
-    PRINTF("0x");
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[8]));
-    PRINTF("'");
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[7]));
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[6]));
-    PRINTF("'");
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[5]));
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[4]));
-    PRINTF("'");
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[3]));
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[2]));
-    PRINTF("'");
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[1]));
-    PRINTF("%02x", static_cast<unsigned int>(deviceId[0]));
-}
-
-
-auto WriteAndReadTestData(fram::Address address) -> void
-{
-    auto nBytesToPrint = 10U;
-
-    PRINTF("\n");
-    PRINTF("Writing %d bytes to address   0x%08x ...\n",
-           static_cast<int>(testDataSize),
-           static_cast<unsigned int>(value_of(address)));
-    auto begin = RODOS::NOW();
-    fram::WriteTo(address, Span(testData), spiTimeout);
-    auto end = RODOS::NOW();
-    PRINTF("  took %d us\n", static_cast<int>((end - begin) / RODOS::MICROSECONDS));
-
-    PRINTF("Reading %d bytes from address 0x%08x ...\n",
-           static_cast<int>(testDataSize),
-           static_cast<unsigned int>(value_of(address)));
-    begin = RODOS::NOW();
-    fram::ReadFrom(address, Span(&readData), spiTimeout);
-    end = RODOS::NOW();
-    PRINTF("  took %d us\n", static_cast<int>((end - begin) / RODOS::MICROSECONDS));
-
-    PRINTF("\n");
-    PRINTF("Comparing first %d written and read bytes:\n", nBytesToPrint);
-    PRINTF("  ");
-    for(auto byte : Span(testData).first(nBytesToPrint))
-    {
-        PRINTF("0x%02x ", static_cast<unsigned char>(byte));
-    }
-    PRINTF("\n  ");
-    for(auto byte : Span(readData).first(nBytesToPrint))
-    {
-        PRINTF("0x%02x ", static_cast<unsigned char>(byte));
-    }
-    PRINTF("\n");
-    PRINTF("Comparing the full arrays ...\n");
-    Check(readData == testData);
-}
+    writtenData.fill(0x00_b);
+    fram::WriteTo(address, Span(writtenData), 30 * ms);
+    fram::ReadFrom(address, Span(&readData), 30 * ms);
+    CHECK(readData == writtenData);
 }
