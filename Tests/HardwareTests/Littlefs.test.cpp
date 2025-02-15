3e6d036307a61020990a34c887e1a527e091757b
Patrick Kappl <kappl.pa@gmail.com>
2025-02-15 13:16:15 +0000

    Rework littlefs test to use CatchRodos

--- Tests/HardwareTests/Littlefs.test.cpp
+++ Tests/HardwareTests/Littlefs.test.cpp
@@ -1,118 +1,66 @@
-#include <Tests/HardwareTests/Utility.hpp>
+#include <Tests/CatchRodos/TestMacros.hpp>
 
 #include <Sts1CobcSw/FileSystem/LfsMemoryDevice.hpp>
 
 #include <littlefs/lfs.h>
 
-#include <rodos_no_using_namespace.h>
-
-#include <cstddef>
+#include <etl/string.h>
 
 
-namespace sts1cobcsw
+namespace fs = sts1cobcsw::fs;
+
+using Path = etl::string<fs::maxPathLength>;
+
+
+TEST_CASE("Littlefs")
 {
-using RODOS::PRINTF;
+    fs::Initialize();
+    auto lfs = lfs_t{};
+    auto errorCode = lfs_format(&lfs, &fs::lfsConfig);
+    CHECK(errorCode == 0);
+    errorCode = lfs_mount(&lfs, &fs::lfsConfig);
+    REQUIRE(errorCode == 0);
 
+    auto const * directoryPath = "MyFolder";
+    errorCode = lfs_mkdir(&lfs, directoryPath);
+    CHECK(errorCode == 0);
+    auto filePath = Path(directoryPath);
+    filePath.append("/MyFile");
+    auto file = lfs_file_t{};
+    errorCode = lfs_file_open(&lfs, &file, filePath.c_str(), LFS_O_WRONLY | LFS_O_CREAT);
+    CHECK(errorCode == 0);
+    static constexpr auto writtenNumber = 123;
+    errorCode = lfs_file_write(&lfs, &file, &writtenNumber, sizeof(writtenNumber));
+    CHECK(errorCode == static_cast<int>(sizeof(writtenNumber)));
+    errorCode = lfs_file_close(&lfs, &file);
+    CHECK(errorCode == 0);
 
-// The minimum required stack size is ~2540 bytes
-constexpr std::size_t stackSize = 2600;
+    errorCode = lfs_unmount(&lfs);
+    CHECK(errorCode == 0);
+    errorCode = lfs_mount(&lfs, &fs::lfsConfig);
+    CHECK(errorCode == 0);
 
+    file = lfs_file_t{};
+    errorCode = lfs_file_open(&lfs, &file, filePath.c_str(), LFS_O_RDONLY);
+    CHECK(errorCode == 0);
+    auto readNumber = 0;
+    errorCode = lfs_file_read(&lfs, &file, &readNumber, sizeof(readNumber));
+    CHECK(errorCode == static_cast<int>(sizeof(readNumber)));
+    CHECK(readNumber == writtenNumber);
+    errorCode = lfs_file_close(&lfs, &file);
+    CHECK(errorCode == 0);
 
-class LittlefsTest : public RODOS::StaticThread<stackSize>
-{
-public:
-    LittlefsTest() : StaticThread("LittlefsTest")
-    {
-    }
+    errorCode = lfs_remove(&lfs, directoryPath);
+    CHECK(errorCode == LFS_ERR_NOTEMPTY);
+    errorCode = lfs_remove(&lfs, "NonExistentFolder/");
+    CHECK(errorCode == LFS_ERR_NOENT);
+    errorCode = lfs_remove(&lfs, "NonExistentFile.txt");
+    CHECK(errorCode == LFS_ERR_NOENT);
+    errorCode = lfs_remove(&lfs, filePath.c_str());
+    CHECK(errorCode == 0);
+    errorCode = lfs_remove(&lfs, directoryPath);
+    CHECK(errorCode == 0);
 
-
-private:
-    auto init() -> void override
-    {
-    }
-
-
-    auto run() -> void override
-    {
-        PRINTF("\n\nlittlefs test\n");
-
-        PRINTF("\n");
-        fs::Initialize();
-        auto lfs = lfs_t{};
-        PRINTF("Formatting ...\n");
-        auto errorCode = lfs_format(&lfs, &fs::lfsConfig);
-        Check(errorCode == 0);
-        PRINTF("Mounting ...\n");
-        errorCode = lfs_mount(&lfs, &fs::lfsConfig);
-        Check(errorCode == 0);
-
-        PRINTF("\n");
-        auto const * directoryPath = "MyFolder";
-        PRINTF("Creating directory '%s' ...\n", directoryPath);
-        errorCode = lfs_mkdir(&lfs, directoryPath);
-        Check(errorCode == 0);
-        PRINTF("\n");
-        auto const * filePath = "MyFolder/MyFile";
-        PRINTF("Creating file '%s' ...\n", filePath);
-        auto file = lfs_file_t{};
-        errorCode = lfs_file_open(&lfs, &file, filePath, LFS_O_WRONLY | LFS_O_CREAT);
-        Check(errorCode == 0);
-        int number = 123;
-        PRINTF("Writing %d to file ...\n", number);
-        errorCode = lfs_file_write(&lfs, &file, &number, sizeof(number));
-        Check(errorCode == sizeof(number));
-        PRINTF("Closing file ...\n");
-        errorCode = lfs_file_close(&lfs, &file);
-        Check(errorCode == 0);
-
-        PRINTF("\n");
-        PRINTF("Unmounting ...\n");
-        errorCode = lfs_unmount(&lfs);
-        Check(errorCode == 0);
-        PRINTF("Mounting again ...\n");
-        errorCode = lfs_mount(&lfs, &fs::lfsConfig);
-        Check(errorCode == 0);
-
-        PRINTF("\n");
-        PRINTF("Opening file '%s' for reading ...\n", filePath);
-        errorCode = lfs_file_open(&lfs, &file, filePath, LFS_O_RDONLY);
-        Check(errorCode == 0);
-        int readNumber = 0;
-        PRINTF("Reading from file ...\n");
-        errorCode = lfs_file_read(&lfs, &file, &readNumber, sizeof(number));
-        Check(errorCode == sizeof(number));
-        PRINTF("Read number = %d == %d\n", readNumber, number);
-        Check(readNumber == number);
-        PRINTF("Closing file ...\n");
-        errorCode = lfs_file_close(&lfs, &file);
-        Check(errorCode == 0);
-
-        PRINTF("\n");
-        PRINTF("Removing the non-empty directory '%s' should fail ...\n", directoryPath);
-        errorCode = lfs_remove(&lfs, directoryPath);
-        PRINTF("Error code = %d == %d\n", errorCode, LFS_ERR_NOTEMPTY);
-        Check(errorCode == LFS_ERR_NOTEMPTY);
-        PRINTF("Removing the file '%s' ...\n", filePath);
-        errorCode = lfs_remove(&lfs, filePath);
-        Check(errorCode == 0);
-        PRINTF("Removing the empty directory '%s' ...\n", directoryPath);
-        errorCode = lfs_remove(&lfs, directoryPath);
-        Check(errorCode == 0);
-
-        PRINTF("\n");
-        PRINTF("Unmounting ...\n");
-        errorCode = lfs_unmount(&lfs);
-        Check(errorCode == 0);
-
-        PRINTF("\n");
-        if(AllChecksPassed())
-        {
-            PRINTF("ALL CHECKS PASSED :)\n");
-        }
-        else
-        {
-            PRINTF("SOME CHECKS FAILED :(\n");
-        }
-    }
-} littlefsTest;
+    errorCode = lfs_unmount(&lfs);
+    CHECK(errorCode == 0);
 }
