5369694dbdaf5cb634586cb761f7110f3eac3e7d
Patrick Kappl <kappl.pa@gmail.com>
2025-02-15 13:28:30 +0000

    Rework RF test to use CatchRodos

--- Tests/HardwareTests/Rf.test.cpp
+++ Tests/HardwareTests/Rf.test.cpp
@@ -1,42 +1,14 @@
-#include <Tests/HardwareTests/Utility.hpp>
+#include <Tests/CatchRodos/TestMacros.hpp>
 
 #include <Sts1CobcSw/Periphery/Rf.hpp>
 
-#include <rodos_no_using_namespace.h>
+
+namespace rf = sts1cobcsw::rf;
 
 
-namespace sts1cobcsw
+TEST_CASE("RF module")
 {
-using RODOS::PRINTF;
-
-
-class RfTest : public RODOS::StaticThread<>
-{
-public:
-    RfTest() : StaticThread("RfTest")
-    {
-    }
-
-
-private:
-    void init() override
-    {
-    }
-
-
-    void run() override
-    {
-        PRINTF("\nRF test\n\n");
-
-        rf::Initialize(rf::TxType::morse);
-        PRINTF("RF module initialized\n");
-
-        PRINTF("\n");
-        auto partNumber = rf::ReadPartNumber();
-        PRINTF("Part number: 0x%4x == 0x%4x\n", partNumber, rf::correctPartNumber);
-        Check(partNumber == rf::correctPartNumber);
-
-        // Here comes the rest of the RF test
-    }
-} rfTest;
+    rf::Initialize(rf::TxType::morse);
+    auto partNumber = rf::ReadPartNumber();
+    CHECK(partNumber == rf::correctPartNumber);
 }
