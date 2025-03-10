b3301938581c1beb3a77d2685c1aaf840f057d49
Patrick Kappl <kappl.pa@gmail.com>
2025-03-10 16:05:59 +0000

    Add topics and buffers for telemetry record

--- Sts1CobcSw/CobcSoftware/TopicsAndSubscribers.hpp
+++ Sts1CobcSw/CobcSoftware/TopicsAndSubscribers.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 
+#include <Sts1CobcSw/ProgramId/ProgramId.hpp>
 #include <Sts1CobcSw/Utility/TimeTypes.hpp>
 
 #include <rodos_no_using_namespace.h>
+
+#include <cstdint>
 
 
 namespace sts1cobcsw
@@ -15,6 +18,16 @@ extern RODOS::CommBuffer<bool> eduIsAliveBufferForListener;
 extern RODOS::CommBuffer<bool> eduIsAliveBufferForPowerManagement;
 extern RODOS::CommBuffer<bool> eduIsAliveBufferForCommunicationError;
 extern RODOS::CommBuffer<bool> eduIsAliveBufferForTelemetry;
+
 extern RODOS::Topic<Duration> nextProgramStartDelayTopic;
 extern RODOS::CommBuffer<Duration> nextProgramStartDelayBuffer;
+
+extern RODOS::Topic<ProgramId> programIdOfCurrentEduProgramQueueEntryTopic;
+extern RODOS::CommBuffer<ProgramId> programIdOfCurrentEduProgramQueueEntryBuffer;
+
+extern RODOS::Topic<std::int32_t> rxBaudRateTopic;
+extern RODOS::CommBuffer<std::int32_t> rxBaudRateBuffer;
+
+extern RODOS::Topic<std::int32_t> txBaudRateTopic;
+extern RODOS::CommBuffer<std::int32_t> txBaudRateBuffer;
 }
