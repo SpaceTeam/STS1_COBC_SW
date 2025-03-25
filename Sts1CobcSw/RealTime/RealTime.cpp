1432544362ed1f5d60cf03920244cf9afb7e4474
Jerome Hue <hue.jerome@yahoo.fr>
2025-03-25 20:23:23 +0100

    Add function to update real time offset

---
+++ Sts1CobcSw/RealTime/RealTime.cpp
@@ -0,0 +1,28 @@
+#include <Sts1CobcSw/FramSections/FramLayout.hpp>
+#include <Sts1CobcSw/FramSections/PersistentVariables.hpp>
+#include <Sts1CobcSw/RealTime/RealTime.hpp>
+#include <Sts1CobcSw/RodosTime/RodosTime.hpp>
+
+#include <strong_type/affine_point.hpp>
+#include <strong_type/difference.hpp>
+#include <strong_type/type.hpp>
+
+#include <rodos_no_using_namespace.h>
+
+#include <climits>
+
+
+namespace sts1cobcsw
+{
+auto UpdateRealTimeOffset(RealTime telecommandTimestamp, std::int32_t rxBaudRate) -> void
+{
+    // TODO: Get this from the CCSDS TC Space Data Link Protocol config once we have it
+    static constexpr auto telecommandSize = 383;
+    auto currentRodosTime = CurrentRodosTime();
+    auto const transmissionDuration = telecommandSize * CHAR_BIT * 1000 / rxBaudRate * ms;
+    auto offsetCorrection = persistentVariables.template Load<"realTimeOffsetCorrection">();
+    auto newRealTimeOffset = RodosTime(value_of(telecommandTimestamp) * RODOS::SECONDS)
+                           + transmissionDuration + offsetCorrection - currentRodosTime;
+    persistentVariables.template Store<"realTimeOffset">(newRealTimeOffset);
+}
+}
--- Sts1CobcSw/RealTime/RealTime.hpp
+++ Sts1CobcSw/RealTime/RealTime.hpp
@@ -4,6 +4,7 @@
 #include <Sts1CobcSw/Vocabulary/Time.hpp>
 
 #include <cinttypes>
+#include <cstdint>
 
 
 #define DEBUG_PRINT_REAL_TIME() DEBUG_PRINT("Real time: %" PRIi32 "\n", value_of(CurrentRealTime()))
@@ -14,6 +15,7 @@ namespace sts1cobcsw
 [[nodiscard]] auto CurrentRealTime() -> RealTime;
 [[nodiscard]] auto ToRodosTime(RealTime realTime) -> RodosTime;
 [[nodiscard]] auto ToRealTime(RodosTime rodosTime) -> RealTime;
+auto UpdateRealTimeOffset(RealTime telecommandTimestamp, std::int32_t rxBaudRate) -> void;
 }
 
 
