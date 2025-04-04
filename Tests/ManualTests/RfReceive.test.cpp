0c6e481e59828d7f53a53a316f18a2dae0d1bbe8
Patrick Kappl <kappl.pa@gmail.com>
2025-04-04 18:24:03 +0000

    Update `RfReceive.test.cpp`

--- Tests/ManualTests/RfReceive.test.cpp
+++ Tests/ManualTests/RfReceive.test.cpp
@@ -3,6 +3,7 @@
 #include <Sts1CobcSw/Hal/GpioPin.hpp>
 #include <Sts1CobcSw/Hal/IoNames.hpp>
 #include <Sts1CobcSw/Rf/Rf.hpp>
+#include <Sts1CobcSw/Utility/Span.hpp>
 
 #include <rodos_no_using_namespace.h>
 
@@ -14,6 +15,9 @@ namespace sts1cobcsw
 using RODOS::PRINTF;
 
 auto led1GpioPin = hal::GpioPin(hal::led1Pin);
+
+
+auto Print(std::span<Byte const> data) -> void;
 
 
 class RfReceiveTest : public RODOS::StaticThread<5'000>
@@ -45,21 +49,49 @@ private:
         if(receiveTestDataResult.has_error())
         {
             PRINTF("Error: %i\n", static_cast<int>(receiveTestDataResult.error()));
+        }
+        else
+        {
+            auto & receivedTestData = receiveTestDataResult.value();
+            PRINTF("Received data:");
+            Print(Span(receivedTestData));
+            PRINTF("-> done\n");
+        }
+
+        // A fully encoded transfer frame is 255 * 1.5 = 382.5 -> 383 bytes long
+        auto receivedData = std::array<Byte, 383>{};
+        static constexpr auto rxTimeout = 5 * s;
+        PRINTF("\n");
+        PRINTF("Waiting %i s to receive %i bytes\n",
+               static_cast<int>(rxTimeout / s),
+               static_cast<int>(receivedData.size()));
+        auto receiveResult = rf::Receive(Span(&receivedData), rxTimeout);
+        if(receiveResult.has_error())
+        {
+            PRINTF("Error: %i\n", static_cast<int>(receiveResult.error()));
             return;
         }
-        auto receivedData = receiveTestDataResult.value();
         PRINTF("Received data:");
-        static constexpr auto valuesPerLine = 100 / 5;
-        for(auto i = 0U; i < receivedData.size(); ++i)
-        {
-            if(i % valuesPerLine == 0)
-            {
-                PRINTF("\n");
-            }
-            PRINTF("0x%02x ", static_cast<unsigned int>(receivedData[i]));
-        }
-        PRINTF("\n");
+        Print(Span(receivedData));
         PRINTF("-> done\n");
     }
 } rfReceiveTest;
+
+
+auto Print(std::span<Byte const> data) -> void
+{
+    static constexpr auto valuesPerLine = 100 / 5;
+    for(auto i = 0U; i < data.size(); ++i)
+    {
+        PRINTF("0x%02x ", static_cast<unsigned int>(data[i]));
+        if(i % valuesPerLine == valuesPerLine - 1)
+        {
+            PRINTF("\n");
+        }
+    }
+    if(data.size() % valuesPerLine != 0)
+    {
+        PRINTF("\n");
+    }
+}
 }
