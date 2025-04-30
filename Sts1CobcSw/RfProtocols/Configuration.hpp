0ad443ae216e824f3f89fc1e97a1b7ecefe45e6b
Patrick Kappl <patrick.kappl@spaceteam.at>
2025-04-30 17:17:18 +0000

    Add `tm::SpacePacketSecondaryHeader`

--- Sts1CobcSw/RfProtocols/Configuration.hpp
+++ Sts1CobcSw/RfProtocols/Configuration.hpp
@@ -2,6 +2,7 @@
 
 
 #include <Sts1CobcSw/RfProtocols/Id.hpp>
+#include <Sts1CobcSw/RfProtocols/MessageTypeIdFields.hpp>
 #include <Sts1CobcSw/Serial/Byte.hpp>
 #include <Sts1CobcSw/Serial/UInt.hpp>
 
@@ -72,4 +73,34 @@ inline constexpr auto normalApid = Make<Apid, 0b000'1100'1100>();
 inline constexpr auto idlePacketApid = Make<Apid, 0x7FF>();
 
 inline constexpr auto idleData = 0x55_b;
+
+
+// --- Packet Utilization Standard ---
+
+using ApplicationProcessUserId = Id<std::uint16_t, 0xAAAA>;  // NOLINT(*magic-numbers)
+inline constexpr auto applicationProcessUserId = Make<ApplicationProcessUserId, 0xAAAA>();
+
+namespace tm
+{
+inline constexpr auto packetSecondaryHeaderLength = 11U;
+inline constexpr auto packetPusVersionNumber = UInt<4>(2);
+
+// NOLINTBEGIN(*magic-numbers)
+using MessageTypeId = Id<MessageTypeIdFields,
+                         MessageTypeIdFields{1, 1},
+                         MessageTypeIdFields{1, 2},
+                         MessageTypeIdFields{1, 7},
+                         MessageTypeIdFields{1, 8},
+                         MessageTypeIdFields{3, 25},
+                         MessageTypeIdFields{6, 6},
+                         MessageTypeIdFields{20, 2},
+                         MessageTypeIdFields{23, 4},
+                         MessageTypeIdFields{23, 13}>;
+// NOLINTEND(*magic-numbers)
+}
+
+namespace tc
+{
+inline constexpr auto packetPusVersionNumber = UInt<4>(2);
+}
 }
