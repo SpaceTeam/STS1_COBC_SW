8a3598266cab0f5cb24b4da23acd3a9bfd18522b
Patrick Kappl <kappl.pa@gmail.com>
2025-05-01 15:01:49 +0000

    Rework `Id<>`
    Remove the invariant that `Id`s can only hold valid values. This allows
    separating deserialization and validation, which simplifies
    deserialization and makes parse functions more consistent.

--- Tests/UnitTests/IdCounters.test.cpp
+++ Tests/UnitTests/IdCounters.test.cpp
@@ -3,29 +3,42 @@
 #include <Sts1CobcSw/RfProtocols/Id.hpp>
 #include <Sts1CobcSw/RfProtocols/IdCounters.hpp>
 
+#include <cstdint>
 
-using MyId = sts1cobcsw::Id<int, 1, 4, 7>;
+
+using sts1cobcsw::Make;
+
+
+struct MyIdFields
+{
+    std::uint8_t minor = 0;
+    std::uint8_t major = 0;
+
+    friend constexpr auto operator==(MyIdFields const &, MyIdFields const &) -> bool = default;
+};
+
+using MyId = sts1cobcsw::Id<MyIdFields, MyIdFields{0, 1}, MyIdFields{3, 5}, MyIdFields{7, 9}>;
 
 
 auto myIdCounters = sts1cobcsw::IdCounters<int, MyId>{};
 
-constexpr auto id1 = MyId::Make<1>();
-constexpr auto id4 = MyId::Make<4>();
-constexpr auto id7 = MyId::Make<7>();
+constexpr auto id1 = Make<MyId, MyIdFields{0, 1}>();
+constexpr auto id2 = Make<MyId, MyIdFields{3, 5}>();
+constexpr auto id3 = Make<MyId, MyIdFields{7, 9}>();
 
 
 TEST_CASE("IdCounters")
 {
     CHECK(myIdCounters.Get(id1) == 0);
-    CHECK(myIdCounters.Get(id4) == 0);
-    CHECK(myIdCounters.Get(id7) == 0);
+    CHECK(myIdCounters.Get(id2) == 0);
+    CHECK(myIdCounters.Get(id3) == 0);
     for(auto i = 0; i < 10; ++i)
     {
         CHECK(myIdCounters.PostIncrement(id1) == i);
-        CHECK(myIdCounters.PostIncrement(id4) == i);
-        CHECK(myIdCounters.PostIncrement(id7) == i);
+        CHECK(myIdCounters.PostIncrement(id2) == i);
+        CHECK(myIdCounters.PostIncrement(id3) == i);
         CHECK(myIdCounters.Get(id1) == i + 1);
-        CHECK(myIdCounters.Get(id4) == i + 1);
-        CHECK(myIdCounters.Get(id7) == i + 1);
+        CHECK(myIdCounters.Get(id2) == i + 1);
+        CHECK(myIdCounters.Get(id3) == i + 1);
     }
 }
