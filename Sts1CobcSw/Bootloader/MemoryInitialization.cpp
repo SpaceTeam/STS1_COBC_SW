6303e199d5e5fe0bbc5ff2d2198bc117f30ffec4
Patrick Kappl <kappl.pa@gmail.com>
2025-05-29 17:46:02 +0000

    Add `.clangd` to disable clang-tidy in bootloader code

--- Sts1CobcSw/Bootloader/MemoryInitialization.cpp
+++ Sts1CobcSw/Bootloader/MemoryInitialization.cpp
@@ -1,11 +1,9 @@
 // Linker symbols to memory segments
-// NOLINTBEGIN(*reserved-identifier, cert-dcl51-cpp, cert-dcl37-c, *identifier-naming)
 extern char * _sidata;
 extern char volatile * _sdata;
 extern char volatile * _edata;
 extern char volatile * _sbss;
 extern char volatile * _ebss;
-// NOLINTEND(*reserved-identifier, cert-dcl51-cpp, cert-dcl37-c, *identifier-naming)
 
 
 extern "C"
@@ -20,7 +18,7 @@ auto InitializeDataSegment() -> void
     auto size = _edata - _sdata;
     for(auto i = 0; i < size; i++)
     {
-        _sdata[i] = _sidata[i];  // NOLINT(*pointer-arithmetic)
+        _sdata[i] = _sidata[i];
     }
 }
 
@@ -30,6 +28,6 @@ auto InitializeBssSegment() -> void
     auto size = _ebss - _sbss;
     for(auto i = 0; i < size; i++)
     {
-        _sbss[i] = 0;  // NOLINT(*pointer-arithmetic)
+        _sbss[i] = 0;
     }
 }
