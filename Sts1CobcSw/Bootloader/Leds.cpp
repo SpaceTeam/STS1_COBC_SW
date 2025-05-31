5b829fb27e13f3d2294cf71bbf90fb59624c57f3
Patrick Kappl <kappl.pa@gmail.com>
2025-05-31 16:56:28 +0000

    Add `Leds.hpp/cpp`

---
+++ Sts1CobcSw/Bootloader/Leds.cpp
@@ -0,0 +1,36 @@
+#include <Sts1CobcSw/Bootloader/Leds.hpp>
+
+#include <Sts1CobcSw/Bootloader/stm32f411xe.h>
+
+
+namespace sts1cobcsw::leds
+{
+auto Initialize() -> void
+{
+    // LEDs are connected to PB12 and PB15
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;                                // Enable GPIOB clock
+    GPIOB->MODER |= (GPIO_MODER_MODER12_0 | GPIO_MODER_MODER15_0);      // Output mode
+    GPIOB->OSPEEDR |= (GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED15);  // High speed
+}
+
+
+auto Reset() -> void
+{
+    // Reset PB12 and PB15 to default state
+    GPIOB->MODER &= ~(GPIO_MODER_MODER12 | GPIO_MODER_MODER15);          // Input mode
+    GPIOB->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED15);  // Low speed
+    RCC->AHB1ENR &= ~RCC_AHB1ENR_GPIOBEN;                                // Disable GPIOB clock
+}
+
+
+auto TurnOn() -> void
+{
+    GPIOB->ODR |= (GPIO_ODR_OD12 | GPIO_ODR_OD15);  // Set PB12 and PB15 high
+}
+
+
+auto TurnOff() -> void
+{
+    GPIOB->ODR &= ~(GPIO_ODR_OD12 | GPIO_ODR_OD15);  // Set PB12 and PB15 low
+}
+}
---
+++ Sts1CobcSw/Bootloader/Leds.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+namespace sts1cobcsw::leds
+{
+auto Initialize() -> void;
+auto Reset() -> void;
+auto TurnOn() -> void;
+auto TurnOff() -> void;
+}
