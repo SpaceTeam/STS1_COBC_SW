ac3d83eb8951e59c7b981538847557403bed2bbe
Patrick Kappl <kappl.pa@gmail.com>
2025-06-05 21:01:17 +0000

    Refactor CMake code
    - Remove `warning_guard` since it's only useful for libraries
    - Remove commented out code
    - Remove `project-is-top-level.cmake` since it's obsolete since CMake
      3.21
    - Add option `USE_CTCACHE`
    - Rename and resturcture some things

---
+++ cmake/options-and-variables.cmake
@@ -0,0 +1,25 @@
+# ---- Options and cache variables ----
+
+option(BUILD_BOOTLOADER "Build the bootloader instead of the firmware" OFF)
+if(BUILD_BOOTLOADER AND CMAKE_SYSTEM_NAME STREQUAL Linux)
+    message(FATAL_ERROR "Building the bootloader is only supported on embedded platforms")
+endif()
+
+set(HW_VERSION 30 CACHE STRING "Hardware version")
+
+# Developer mode enables targets and code paths in the CMake scripts that are only relevant for the
+# developer(s) of STS1 COBC SW. Targets necessary to build the project must be provided
+# unconditionally, so consumers can trivially build and package the project.
+if(PROJECT_IS_TOP_LEVEL)
+    option(Sts1CobcSw_DEVELOPER_MODE "Enable developer mode" OFF)
+endif()
+
+# ---- Normal (non-cache) variables ----
+
+if(CMAKE_BUILD_TYPE STREQUAL "Sanitize")
+    set(EXTRA_SANITIZER_STACK_SIZE 20000)
+else()
+    set(EXTRA_SANITIZER_STACK_SIZE 0)
+endif()
+message("Adding -DEXTRA_SANITIZER_STACK_SIZE=${EXTRA_SANITIZER_STACK_SIZE} to all targets")
+add_compile_definitions(EXTRA_SANITIZER_STACK_SIZE=${EXTRA_SANITIZER_STACK_SIZE})
