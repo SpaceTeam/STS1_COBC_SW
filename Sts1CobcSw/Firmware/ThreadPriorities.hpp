dbf847b6a5313467b9ecdd26885f1ba6a16c75d1
Patrick Kappl <kappl.pa@gmail.com>
2025-06-16 20:21:30 +0000

    Remove deprecated command parser and command parser thread

--- Sts1CobcSw/Firmware/ThreadPriorities.hpp
+++ Sts1CobcSw/Firmware/ThreadPriorities.hpp
@@ -9,7 +9,6 @@ namespace sts1cobcsw
 inline constexpr auto framEpsStartupTestThreadPriority = 97;
 inline constexpr auto flashStartupTestThreadPriority = 98;
 inline constexpr auto rfStartupTestThreadPriority = 99;
-inline constexpr auto commandParserThreadPriority = 100;
 inline constexpr auto eduListenerThreadPriority = 210;
 inline constexpr auto eduProgramQueueThreadPriority = 230;
 inline constexpr auto eduCommunicationErrorThreadPriority = 240;
