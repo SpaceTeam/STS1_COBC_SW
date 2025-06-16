681b718484372c225b6a563469f5b040682394d0
Patrick Kappl <kappl.pa@gmail.com>
2025-06-16 20:17:27 +0000

    Fix redirecting RF over UCI
    - Do not change the UART baud rate with `SetRx/TxDataRate()`, because
      that causes issues, specifically, reinitializing the UART every time
      we send or receive something does
    - Send 0x02 to signal the start of a frame
    - Add a line break after sending or receiving a frame for nicer
      formatting
    - Fix CMake code

--- Sts1CobcSw/Rf/RfOverUci.cpp
+++ Sts1CobcSw/Rf/RfOverUci.cpp
@@ -1,12 +1,14 @@
 #include <Sts1CobcSw/Hal/IoNames.hpp>
 #include <Sts1CobcSw/Hal/Uart.hpp>
 #include <Sts1CobcSw/Rf/Rf.hpp>  // IWYU pragma: associated
+#include <Sts1CobcSw/Utility/Span.hpp>
 
 #include <strong_type/difference.hpp>
 #include <strong_type/type.hpp>
 
 #include <rodos_no_using_namespace.h>
 
+#include <array>
 #include <utility>
 
 
@@ -14,18 +16,28 @@ namespace sts1cobcsw::rf
 {
 namespace
 {
-constexpr auto defaultDataRate = 115'200U;
+// For more convenience during tests, we will not change the baud rate of the UCI UART with
+// SetTx/RxDataRate(). We'll just store the RX and TX data rates and put them in the telemetry
+// record.
+constexpr auto uartBaudRate = 115'200U;
+// To be able to distinguish between sending an RF frame and a string from RODOS::PRINTF() over the
+// UART, we add the ASCII control character 0x02 = STX (Start of Text) at the start of each frame.
+// We also add a linebreak at the end of each frame, for nicer formatting in HTerm.
+constexpr auto startOfFrame = 0x02_b;
+constexpr auto endOfFrame = std::array{0x0D_b, 0x0A_b};  // = \r\n
+constexpr auto frameDelimiterTimeout = 10 * ms;  // Timeout for writing the start or end of frame
 
 auto uciUart = RODOS::HAL_UART(hal::uciUartIndex, hal::uciUartTxPin, hal::uciUartRxPin);
+// FIXME: There is actually a persistent variable for this: "txIsOn"
 auto txIsEnabled = true;
-auto rxDataRate = defaultDataRate;
-auto txDataRate = defaultDataRate;
+auto rxDataRate = uartBaudRate;
+auto txDataRate = uartBaudRate;
 }
 
 
 auto Initialize([[maybe_unused]] TxType txType) -> void
 {
-    hal::Initialize(&uciUart, defaultDataRate);
+    hal::Initialize(&uciUart, uartBaudRate);
 }
 
 
@@ -87,12 +99,16 @@ auto SendAndWait(std::span<Byte const> data) -> Result<void>
 {
     if(not txIsEnabled)
     {
-        // TODO: Think about what to do if TX is disabled
         return outcome_v2::success();
     }
-    hal::Initialize(&uciUart, txDataRate);
-    auto const timeout = static_cast<int>(data.size()) * s / txDataRate + 20 * ms;
-    return hal::WriteTo(&uciUart, data, timeout);
+    auto const txByteRate = txDataRate / 10;
+    static constexpr auto safetyFactor = 2;
+    static constexpr auto safetyMargin = 10 * ms;
+    auto const timeout =
+        static_cast<int>(data.size()) * s / txByteRate * safetyFactor + safetyMargin;
+    OUTCOME_TRY(hal::WriteTo(&uciUart, Span(startOfFrame), frameDelimiterTimeout));
+    OUTCOME_TRY(hal::WriteTo(&uciUart, data, timeout));
+    return (hal::WriteTo(&uciUart, Span(endOfFrame), frameDelimiterTimeout));
 }
 
 
@@ -114,7 +130,9 @@ auto SuspendUntilDataSent([[maybe_unused]] Duration timeout) -> Result<void>
 
 auto Receive(std::span<Byte> data, Duration timeout) -> Result<void>
 {
-    hal::Initialize(&uciUart, rxDataRate);
-    return hal::ReadFrom(&uciUart, data, timeout);
+    auto result = hal::ReadFrom(&uciUart, data, timeout);
+    // Add a line break after receiving the data, for nicer formatting in HTerm.
+    OUTCOME_TRY(hal::WriteTo(&uciUart, Span(endOfFrame), frameDelimiterTimeout));
+    return result;
 }
 }
