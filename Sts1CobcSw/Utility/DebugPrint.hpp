0263fb29c0384307a6ff405589bc38a93c21c0a7
Patrick Kappl <kappl.pa@gmail.com>
2025-06-29 16:39:07 +0000

    Add CMake option `ENABLE_DEBUG_PRINT_STACK_USAGE`

--- Sts1CobcSw/Utility/DebugPrint.hpp
+++ Sts1CobcSw/Utility/DebugPrint.hpp
@@ -9,13 +9,15 @@
     #pragma GCC diagnostic ignored "-Wpedantic"
     // NOLINTNEXTLINE(cppcoreguidelines-macro-usage, *variadic-macro-arguments)
     #define DEBUG_PRINT(fmt, ...) RODOS::PRINTF(fmt, ##__VA_ARGS__)
-    #define DEBUG_PRINT_STACK_USAGE() \
-        RODOS::PRINTF("[%s#%i] max. stack usage = %5u B\n", \
-                      __FILE_NAME__, \
-                      __LINE__, \
-                      getMaxStackUsage())
     #pragma GCC diagnostic pop
 #else
     #define DEBUG_PRINT(fmt, ...)
+#endif
+
+#ifdef ENABLE_DEBUG_PRINT_STACK_USAGE
+    #define DEBUG_PRINT_STACK_USAGE() \
+        DEBUG_PRINT( \
+            "[%s#%i] max. stack usage = %5u B\n", __FILE_NAME__, __LINE__, getMaxStackUsage())
+#else
     #define DEBUG_PRINT_STACK_USAGE()
 #endif
