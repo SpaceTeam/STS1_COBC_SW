0da8a09b1d72a4dec4895e7e192835e74d296209
Patrick Kappl <kappl.pa@gmail.com>
2025-06-29 09:15:48 +0000

    Add `DEBUG_PRINT_STACK_USAGE()`

--- Sts1CobcSw/Utility/DebugPrint.hpp
+++ Sts1CobcSw/Utility/DebugPrint.hpp
@@ -9,7 +9,13 @@
     #pragma GCC diagnostic ignored "-Wpedantic"
     // NOLINTNEXTLINE(cppcoreguidelines-macro-usage, *variadic-macro-arguments)
     #define DEBUG_PRINT(fmt, ...) RODOS::PRINTF(fmt, ##__VA_ARGS__)
+    #define DEBUG_PRINT_STACK_USAGE() \
+        RODOS::PRINTF("[%s#%i] max. stack usage = %5u B\n", \
+                      __FILE_NAME__, \
+                      __LINE__, \
+                      getMaxStackUsage())
     #pragma GCC diagnostic pop
 #else
     #define DEBUG_PRINT(fmt, ...)
+    #define DEBUG_PRINT_STACK_USAGE()
 #endif
