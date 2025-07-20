34eb0d6cae2e7a1cdfa64c5383a9eb020aaf6adf
Patrick Kappl <kappl.pa@gmail.com>
2025-07-20 10:28:12 +0000

    Start EDU program queue thread before power management thread

---
+++ Sts1CobcSw/Firmware/EduPowerManagementThread.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include <Sts1CobcSw/Vocabulary/Time.hpp>
+
+#include <strong_type/difference.hpp>
+#include <strong_type/type.hpp>
+
+#include <utility>
+
+
+namespace sts1cobcsw
+{
+inline constexpr auto eduPowerManagementThreadStartDelay = 15 * s;
+}
