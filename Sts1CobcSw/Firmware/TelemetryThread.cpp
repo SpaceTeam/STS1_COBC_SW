8b5d840b70ab29f8c62841a57bf8f0ebc2a5eaf8
Florian Donauer <florian@donauer.at>
2026-09-10 18:30:41 +0000

    Change `initialTelemetryDelay` from 1 min to 1 s

--- Sts1CobcSw/Firmware/TelemetryThread.cpp
+++ Sts1CobcSw/Firmware/TelemetryThread.cpp
@@ -45,7 +45,7 @@ constexpr auto stackSize = 1200U;
 constexpr auto telemetryThreadInterval = 30 * s;
 // The delay until the first telemetry record is published and thereby the first beacon is sent.
 // This prevents immediate resets if we boot with too little power for the RF module.
-constexpr auto initialTelemetryDelay = 1 * min;
+constexpr auto initialTelemetryDelay = 1 * s;
 // The time after which we enable resets in case of firmware corruption. This prevents immediate
 // resets after booting into a corrupt firmware.
 constexpr auto firmwareCorruptionResetEnableTime = RodosTime(0) + 10 * min;
